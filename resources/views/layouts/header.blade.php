d473a69384bbe3644094cd1a838a0f018fba92f9
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-06-25 15:52:43 +0800

    done edit schedule

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -489,8 +489,6 @@
                             <ul class="nav flex-column sub-menu">
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/holidays') }}">Holidays</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/schedules') }}">Schedules</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="{{ url('/allowances') }}">Allowances</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="{{ url('/incentives') }}">Incentives</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/handbooks') }}">Handbook</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/leavee-settings') }}">Leave Type</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/announcements') }}">Announcements</a></li>
@@ -582,6 +580,8 @@
                         <a class="nav-link" href="{{ url('/employee-earned-leaves') }}">Employee Earned Leaves</a>
                     </li>
                     @endif
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('/allowances') }}">Allowances</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('/incentives') }}">Incentives</a></li>
                 </ul>
             </div>
         </li>
@@ -636,7 +636,10 @@
           </div>
         </li>
         <li class="nav-item">
-          <a href="{{url('daily-schedule')}}" class="nav-link">Schedule</a>
+          <a href="{{url('daily-schedule')}}" class="nav-link">
+            <i class="fa fa-calendar menu-icon"></i>
+            Daily Schedule
+          </a>
         <li>
         @endif
         @endif
