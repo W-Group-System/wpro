f18a24cea71c4527d2a3d0aadc33637a55c55ac7
Renz Christian Cabato <cabato.renz.renz@gmail.com>
2024-06-26 10:59:34 +0800

    Payroll View

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -491,7 +491,7 @@
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/schedules') }}">Schedules</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/allowances') }}">Allowances</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/incentives') }}">Incentives</a></li>
-                                <li class="nav-item"> <a class="nav-link" href="{{ url('/handbooks') }}">Handbook</a></li>
+                                {{-- <li class="nav-item"> <a class="nav-link" href="{{ url('/handbooks') }}">Handbook</a></li> --}}
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/leavee-settings') }}">Leave Type</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/announcements') }}">Announcements</a></li>
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/logos') }}">Logos</a></li>
@@ -499,6 +499,8 @@
                             </ul>
                         </div>
                     </li>
+                    @endif
+                    @if (checkUserPrivilege('payroll_view',auth()->user()->id) == 'yes')
                     <li class="nav-item @if ($header == 'Payroll') active @endif">
                         <a class="nav-link" data-toggle="collapse" href="#payroll" aria-expanded="false" aria-controls="ui-basic">
                             <i class="icon-align-center menu-icon"></i>
@@ -512,7 +514,7 @@
                                 <li class="nav-item"> <a class="nav-link" href="{{ url('/loan-reg') }}">Loan Register</a></li>
                             </ul>
                         </div>
-        </li>
+                    </li>
                     @endif
 
                     
