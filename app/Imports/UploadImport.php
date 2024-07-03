ba753fa6d1da4590bb72b73730e7366036161d31
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-03 11:25:36 +0800

    add dtr

--- app/Imports/UploadImport.php
+++ app/Imports/UploadImport.php
@@ -3,6 +3,7 @@
 namespace App\Imports;
 
 use App\Approver;
+use App\AttendanceLog;
 use App\Employee;
 use App\EmployeeApprover;
 use App\EmployeeLeave;
@@ -13,13 +14,14 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class UploadImport implements ToCollection, WithHeadingRow
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     // public function collection(Collection $collection)
     // {
     //   foreach($collection)
@@ -27,107 +29,130 @@ class UploadImport implements ToCollection, WithHeadingRow
     protected $type;
     public function __construct($type)
     {
-      $this->type = $type;
+        $this->type = $type;
     }
 
-    public function collection(Collection $collection) {
-      $rows = $collection->toArray();
-      
-      if (!array_filter($rows)) {
-        return null;
-      }
-      
-      foreach($rows as $row) {
-        $user_id = Employee::where('employee_number', $row['employee_no'])->pluck('user_id')->toArray();
-        $dateFiled = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_filed'])->format('Y-m-d');
+    public function collection(Collection $collection)
+    {
+        $rows = $collection->toArray();
 
-        if ($this->type == "OB") {
-          $employeeOb = EmployeeOb::whereIn('user_id', $user_id)
-            ->where('applied_date', $dateFiled)
-            ->first();
-          
-          foreach($user_id as $uid) {
-            if(empty($employeeOb)) {
-              $employeeOb = new EmployeeOb;
-              $employeeOb->user_id = $uid;
-              $employeeOb->applied_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_filed'])->format('Y-m-d');
-              $employeeOb->date_from = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d').':'.date('H:i:s', strtotime($row['start_time']));
-              $employeeOb->date_to = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d').':'.date('H:i:s', strtotime($row['end_time']));
-              $employeeOb->approved_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
-              $employeeOb->status = $row['status'];
-              $employeeOb->created_by = $uid;
-              $employeeOb->save();
-            }
-            else {
-              $employeeOb->date_from = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d').':'.date('H:i:s', strtotime($row['start_time']));
-              $employeeOb->date_to = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d').':'.date('H:i:s', strtotime($row['end_time']));
-              $employeeOb->approved_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
-              $employeeOb->status = $row['status'];
-              $employeeOb->created_by = $uid;
-              $employeeOb->save();
-            }
-          }
+        if (!array_filter($rows)) {
+            return null;
         }
-        else if ($this->type == "OT") {
-          $employeeOt = EmployeeOvertime::whereIn('user_id', $user_id)->where('ot_date', $dateFiled)->first();
 
-          foreach($user_id as $uid) {
-            if (empty($employeeOt)) {
-              $employeeOt = new EmployeeOvertime;
-              $employeeOt->user_id = $uid;
-              $employeeOt->ot_date = $dateFiled;
-              $employeeOt->start_time = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date_time'])->format('Y-m-d H:i:s');
-              $employeeOt->end_time = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date_time'])->format('Y-m-d H:i:s');
-              $employeeOt->approved_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
-              $employeeOt->status = $row['status'];
-              $employeeOt->created_by = $uid;
-              $employeeOt->save();
-            }
-            else {
-              $employeeOt->start_time = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date_time'])->format('Y-m-d H:i:s');
-              $employeeOt->end_time = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date_time'])->format('Y-m-d H:i:s');
-              $employeeOt->approved_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
-              $employeeOt->status = $row['status'];
-              $employeeOt->created_by = $uid;
-              $employeeOt->save();
-            }
-          }
-        }
-        else if ($this->type == "VL/SL") {
-          $types = 0;
-          if ($row['leave_name'] == "Vacation Leave" || $row['leave_name'] == "VL") {
-            $types = 1;
-          }
-          if ($row['leave_name'] == "Sick Leave" || $row['leave_name'] == 'SL') {
-            $types = 2;
-          }
-          
-          $startDate = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
-          $endDate = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d');
-
-          $leaves = EmployeeLeave::whereIn('user_id', $user_id)
-            ->where('date_from', $startDate)
-            ->where('date_to', $endDate)
-            ->first();
-
-          foreach($user_id as $uid) {
-            if (!empty($leaves)) {
-              $leaves = $leaves->delete();
-            }
+        foreach ($rows as $row) {
             
-            $leaves = new EmployeeLeave;
-            $leaves->user_id = $uid;
-            $leaves->leave_type = $types;
-            $leaves->date_from = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
-            $leaves->date_to = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d');
-            $leaves->approved_date = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
-            $leaves->status = $row['status'];
-            $leaves->created_by = $uid;
-            $leaves->save();
-          }
-        }
-      }
+            if(isset($row['employee_no'])) {
+                $user_id = Employee::where('employee_number', $row['employee_no'])->pluck('user_id')->toArray();
+                $dateFiled = Date::excelToDateTimeObject($row['date_filed'])->format('Y-m-d');
+            }
 
-      Alert::success('Successfully Uploaded')->persistent('Dismiss');
+            if ($this->type == "OB") {
+                $employeeOb = EmployeeOb::whereIn('user_id', $user_id)
+                    ->where('applied_date', $dateFiled)
+                    ->first();
+
+                foreach ($user_id as $uid) {
+                    if (empty($employeeOb)) {
+                        $employeeOb = new EmployeeOb;
+                        $employeeOb->user_id = $uid;
+                        $employeeOb->applied_date = Date::excelToDateTimeObject($row['date_filed'])->format('Y-m-d');
+                        $employeeOb->date_from = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d') . ':' . date('H:i:s', strtotime($row['start_time']));
+                        $employeeOb->date_to = Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d') . ':' . date('H:i:s', strtotime($row['end_time']));
+                        $employeeOb->approved_date = Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
+                        $employeeOb->status = $row['status'];
+                        $employeeOb->created_by = $uid;
+                        $employeeOb->save();
+                    } else {
+                        $employeeOb->date_from = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d') . ':' . date('H:i:s', strtotime($row['start_time']));
+                        $employeeOb->date_to = Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d') . ':' . date('H:i:s', strtotime($row['end_time']));
+                        $employeeOb->approved_date = Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
+                        $employeeOb->status = $row['status'];
+                        $employeeOb->created_by = $uid;
+                        $employeeOb->save();
+                    }
+                }
+            } else if ($this->type == "OT") {
+                $employeeOt = EmployeeOvertime::whereIn('user_id', $user_id)->where('ot_date', $dateFiled)->first();
+
+                foreach ($user_id as $uid) {
+                    if (empty($employeeOt)) {
+                        $employeeOt = new EmployeeOvertime;
+                        $employeeOt->user_id = $uid;
+                        $employeeOt->ot_date = $dateFiled;
+                        $employeeOt->start_time = Date::excelToDateTimeObject($row['start_date_time'])->format('Y-m-d H:i:s');
+                        $employeeOt->end_time = Date::excelToDateTimeObject($row['end_date_time'])->format('Y-m-d H:i:s');
+                        $employeeOt->approved_date = Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
+                        $employeeOt->status = $row['status'];
+                        $employeeOt->created_by = $uid;
+                        $employeeOt->save();
+                    } else {
+                        $employeeOt->start_time = Date::excelToDateTimeObject($row['start_date_time'])->format('Y-m-d H:i:s');
+                        $employeeOt->end_time = Date::excelToDateTimeObject($row['end_date_time'])->format('Y-m-d H:i:s');
+                        $employeeOt->approved_date = Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
+                        $employeeOt->status = $row['status'];
+                        $employeeOt->created_by = $uid;
+                        $employeeOt->save();
+                    }
+                }
+            } else if ($this->type == "VL/SL") {
+                $types = 0;
+                if ($row['leave_name'] == "Vacation Leave" || $row['leave_name'] == "VL") {
+                    $types = 1;
+                }
+                if ($row['leave_name'] == "Sick Leave" || $row['leave_name'] == 'SL') {
+                    $types = 2;
+                }
+
+                $startDate = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
+                $endDate = Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d');
+
+                $leaves = EmployeeLeave::whereIn('user_id', $user_id)
+                    ->where('date_from', $startDate)
+                    ->where('date_to', $endDate)
+                    ->first();
+
+                foreach ($user_id as $uid) {
+                    if (!empty($leaves)) {
+                        $leaves = $leaves->delete();
+                    }
+
+                    $leaves = new EmployeeLeave;
+                    $leaves->user_id = $uid;
+                    $leaves->leave_type = $types;
+                    $leaves->date_from = Date::excelToDateTimeObject($row['start_date'])->format('Y-m-d');
+                    $leaves->date_to = Date::excelToDateTimeObject($row['end_date'])->format('Y-m-d');
+                    $leaves->approved_date = Date::excelToDateTimeObject($row['date_approved'])->format('Y-m-d');
+                    $leaves->status = $row['status'];
+                    $leaves->created_by = $uid;
+                    $leaves->save();
+                }
+            }
+            else if ($this->type == "DTR") {
+                $dateTime = Date::excelToDateTimeObject($row['date_time'])->format('Y-m-d H:i:s');
+                
+                $attendanceLogs = AttendanceLog::where('emp_code', $row['bio_number'])->where('datetime', $dateTime)->first();
+                
+                if (empty($attendanceLogs)) {
+                    $attendanceLogs = new AttendanceLog;
+                    $attendanceLogs->emp_code = $row['bio_number'];
+                    $attendanceLogs->date = Date::excelToDateTimeObject($row['date_time'])->format('Y-m-d');
+                    $attendanceLogs->datetime = $dateTime;
+                    $attendanceLogs->type = strtoupper($row['status']) == 'IN' ? 1 : 0;
+                    $attendanceLogs->location = "DTR";
+                    $attendanceLogs->ip_address = '192.168.14.80';
+                    $attendanceLogs->save();
+                }
+                else {
+                    $attendanceLogs->emp_code = $row['bio_number'];
+                    $attendanceLogs->date = Date::excelToDateTimeObject( $row['date_time'])->format('Y-m-d');
+                    $attendanceLogs->datetime = $dateTime;
+                    $attendanceLogs->type = strtoupper($row['status']) == 'IN' ? 1 : 0;
+                    $attendanceLogs->save();
+                }
+            }
+        }
+
+        Alert::success('Successfully Uploaded')->persistent('Dismiss');
     }
 }
