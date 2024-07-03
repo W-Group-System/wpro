a5b2ab38eb25857f1fdac5a3d3f8bb6090debaef
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-03 14:17:53 +0800

    fix

--- resources/views/schedules/daily-schedule-template.blade.php
+++ resources/views/schedules/daily-schedule-template.blade.php
@@ -1,16 +1,16 @@
 <table>
-  <thead>
-    <tr>
-      <th>company</th>
-      <th>employee_number</th>
-      <th>employee_code</th>
-      <th>employee_name</th>
-      <th>log_date</th>
-      <th>time_in_from</th>
-      <th>time_in_to</th>
-      <th>time_out_from</th>
-      <th>time_out_to</th>
-      <th>working_hours</th>
-    </tr>
-  </thead>
+    <thead>
+        <tr>
+            <th style="background-color: #0080c0; color:black;">Company</th>
+            {{-- <th style="background-color: #0080c0; color:black;">Biometric Number</th> --}}
+            <th style="background-color: #0080c0; color:black;">Employee No.</th>
+            <th style="background-color: #0080c0; color:black;">Employee Name</th>
+            <th style="background-color: #0080c0; color:black;">Log Date</th>
+            <th style="background-color: #0080c0; color:black;">Time In From</th>
+            <th style="background-color: #0080c0; color:black;">Time In To</th>
+            <th style="background-color: #0080c0; color:black;">Time Out From</th>
+            <th style="background-color: #0080c0; color:black;">Time Out To</th>
+            <th style="background-color: #0080c0; color:black;">Working Hours</th>
+        </tr>
+    </thead>
 </table>
