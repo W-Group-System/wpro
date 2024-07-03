8dd72818b2f508a130db3925b8ddc2d24d7fe8f5
JunJihad <junjihadbarroga@gmail.com>
2024-07-03 15:49:46 +0800

    Fix Bug on create salary nopa

--- resources/views/employees/create_salary_nopa.blade.php
+++ resources/views/employees/create_salary_nopa.blade.php
@@ -1,19 +1,19 @@
 <div class="modal fade" id="createSalaryNopa" tabindex="-1" role="dialog" aria-labelledby="createSalaryNopaInfoLabel" aria-hidden="true">
-	<div class="modal-dialog modal-xl" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="createSalaryNopaLabel">Create Salary Notice of Personnel Action</h5>
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-					<span aria-hidden="true">&times;</span>
-				</button>
-			</div>
-			<form method='POST' action='updateEmpSalaryMovementHR/{{ $user->employee->employee_salary->id }}' onsubmit='show()' enctype="multipart/form-data">
-				@csrf
-				<div class="modal-body">
-					<div class="form-card">
-						<div class='row mb-2'>
-							<div class='col-lg-12 mt-1'>
-								<div class="table-responsive">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="createSalaryNopaLabel">Create Salary Notice of Personnel Action</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form method='POST' action='updateEmpSalaryMovementHR/{{ optional($user->employee->employee_salary)->id }}' onsubmit='show()' enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-card">
+                        <div class='row mb-2'>
+                            <div class='col-lg-12 mt-1'>
+                                <div class="table-responsive">
                                     <strong>Salary Details</strong>
                                     <table class="table table-hover">
                                         <thead>
@@ -39,7 +39,7 @@
                                                 </td>
                                                 <td>
                                                     <div class='col-md-12'>
-                                                        <input id="basic_salary_from" class="form-control" type="text" name="basic_salary_from" value="{{$user->employee->employee_salary->basic_salary}}" readonly>
+                                                        <input id="basic_salary_from" class="form-control" type="text" name="basic_salary_from" value="{{ optional($user->employee->employee_salary)->basic_salary ?? 'empty' }}" readonly>
                                                     </div>
                                                 </td>
                                                 <td>
@@ -59,7 +59,7 @@
                                                 </td>
                                                 <td>
                                                     <div class='col-md-12'>
-                                                        <input id="de_minimis_from" class="form-control" type="text" name="de_minimis_from" value="{{$user->employee->employee_salary->de_minimis}}" readonly>
+                                                        <input id="de_minimis_from" class="form-control" type="text" name="de_minimis_from" value="{{ optional($user->employee->employee_salary)->de_minimis ?? 'empty' }}" readonly>
                                                     </div>
                                                 </td>
                                                 <td>
@@ -79,7 +79,7 @@
                                                 </td>
                                                 <td>
                                                     <div class='col-md-12'>
-                                                        <input id="other_allowance_from" class="form-control" type="text" name="other_allowance_from" value="{{$user->employee->employee_salary->other_allowance}}" readonly>
+                                                        <input id="other_allowance_from" class="form-control" type="text" name="other_allowance_from" value="{{ optional($user->employee->employee_salary)->other_allowance ?? 'empty' }}" readonly>
                                                     </div>
                                                 </td>
                                                 <td>
@@ -124,87 +124,85 @@
                                         </tbody>
                                     </table>
                                 </div>
-							</div>
-						</div>
-					</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>    
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
-			</form>
-		</div>
-	</div>
+            </form>
+        </div>
+    </div>
 </div>
 <script>
     function updateTotals() {
-    var basicSalaryFrom = parseFloat(document.getElementById('basic_salary_from').value) || 0;
-    var basicSalaryTo = parseFloat(document.getElementById('basic_salary_to').value) || 0;
-    var deMinimisFrom = parseFloat(document.getElementById('de_minimis_from').value) || 0;
-    var deMinimisTo = parseFloat(document.getElementById('de_minimis_to').value) || 0;
-    var otherAllowanceFrom = parseFloat(document.getElementById('other_allowance_from').value) || 0;
-    var otherAllowanceTo = parseFloat(document.getElementById('other_allowance_to').value) || 0;
+        var basicSalaryFrom = parseFloat(document.getElementById('basic_salary_from').value) || 0;
+        var basicSalaryTo = parseFloat(document.getElementById('basic_salary_to').value) || 0;
+        var deMinimisFrom = parseFloat(document.getElementById('de_minimis_from').value) || 0;
+        var deMinimisTo = parseFloat(document.getElementById('de_minimis_to').value) || 0;
+        var otherAllowanceFrom = parseFloat(document.getElementById('other_allowance_from').value) || 0;
+        var otherAllowanceTo = parseFloat(document.getElementById('other_allowance_to').value) || 0;
 
-    var totalFrom = basicSalaryFrom + deMinimisFrom + otherAllowanceFrom;
-    var totalTo = basicSalaryTo + deMinimisTo + otherAllowanceTo;
+        var totalFrom = basicSalaryFrom + deMinimisFrom + otherAllowanceFrom;
+        var totalTo = basicSalaryTo + deMinimisTo + otherAllowanceTo;
 
-    document.getElementById('total_from').value = totalFrom.toFixed(2);
-    document.getElementById('total_to').value = totalTo.toFixed(2);
+        document.getElementById('total_from').value = totalFrom.toFixed(2);
+        document.getElementById('total_to').value = totalTo.toFixed(2);
 
-    var totalIncreaseField = document.getElementById('total_increase');
-    if (totalFrom > 0 && totalTo > 0) {
-        var totalIncrease = ((totalTo - totalFrom) / totalFrom) * 100;
-        totalIncreaseField.value = totalIncrease.toFixed(2);
-    } else {
-        totalIncreaseField.value = '0';
+        var totalIncreaseField = document.getElementById('total_increase');
+        if (totalFrom > 0 && totalTo > 0) {
+            var totalIncrease = ((totalTo - totalFrom) / totalFrom) * 100;
+            totalIncreaseField.value = totalIncrease.toFixed(2);
+        } else {
+            totalIncreaseField.value = '0';
+        }
     }
-}
 
-document.getElementById('basic_salary_to').addEventListener('input', function() {
-    var basicSalaryFrom = parseFloat(document.getElementById('basic_salary_from').value) || 0;
-    var basicSalaryTo = parseFloat(this.value) || 0;
-    var increaseField = document.getElementById('basic_increase');
+    document.getElementById('basic_salary_to').addEventListener('input', function() {
+        var basicSalaryFrom = parseFloat(document.getElementById('basic_salary_from').value) || 0;
+        var basicSalaryTo = parseFloat(this.value) || 0;
+        var increaseField = document.getElementById('basic_increase');
 
-    if (basicSalaryFrom > 0 && basicSalaryTo > 0) {
-        var increase = ((basicSalaryTo - basicSalaryFrom) / basicSalaryFrom) * 100;
-        increaseField.value = increase.toFixed(2);
-    } else {
-        increaseField.value = '0';
-    }
-    updateTotals();
-});
+        if (basicSalaryFrom > 0 && basicSalaryTo > 0) {
+            var increase = ((basicSalaryTo - basicSalaryFrom) / basicSalaryFrom) * 100;
+            increaseField.value = increase.toFixed(2);
+        } else {
+            increaseField.value = '0';
+        }
+        updateTotals();
+    });
 
-document.getElementById('de_minimis_to').addEventListener('input', function() {
-    var deMinimisFrom = parseFloat(document.getElementById('de_minimis_from').value) || 0;
-    var deMinimisTo = parseFloat(this.value) || 0;
-    var deminimisIncreaseField = document.getElementById('de_minimis_increase');
+    document.getElementById('de_minimis_to').addEventListener('input', function() {
+        var deMinimisFrom = parseFloat(document.getElementById('de_minimis_from').value) || 0;
+        var deMinimisTo = parseFloat(this.value) || 0;
+        var deminimisIncreaseField = document.getElementById('de_minimis_increase');
 
-    if (deMinimisFrom > 0 && deMinimisTo > 0) {
-        var increase = ((deMinimisTo - deMinimisFrom) / deMinimisFrom) * 100;
-        deminimisIncreaseField.value = increase.toFixed(2);
-    } else {
-        deminimisIncreaseField.value = '0';
-    }
-    updateTotals();
-});
+        if (deMinimisFrom > 0 && deMinimisTo > 0) {
+            var increase = ((deMinimisTo - deMinimisFrom) / deMinimisFrom) * 100;
+            deminimisIncreaseField.value = increase.toFixed(2);
+        } else {
+            deminimisIncreaseField.value = '0';
+        }
+        updateTotals();
+    });
 
-document.getElementById('other_allowance_to').addEventListener('input', function() {
-    var otherAllowanceFrom = parseFloat(document.getElementById('other_allowance_from').value) || 0;
-    var otherAllowanceTo = parseFloat(this.value) || 0;
-    var otherAllowanceIncreaseField = document.getElementById('other_allowance_increase');
+    document.getElementById('other_allowance_to').addEventListener('input', function() {
+        var otherAllowanceFrom = parseFloat(document.getElementById('other_allowance_from').value) || 0;
+        var otherAllowanceTo = parseFloat(this.value) || 0;
+        var otherAllowanceIncreaseField = document.getElementById('other_allowance_increase');
 
-    if (otherAllowanceFrom > 0 && otherAllowanceTo > 0) {
-        var increase = ((otherAllowanceTo - otherAllowanceFrom) / otherAllowanceFrom) * 100;
-        otherAllowanceIncreaseField.value = increase.toFixed(2);
-    } else {
-        otherAllowanceIncreaseField.value = '0';
-    }
-    updateTotals();
-});
+        if (otherAllowanceFrom > 0 && otherAllowanceTo > 0) {
+            var increase = ((otherAllowanceTo - otherAllowanceFrom) / otherAllowanceFrom) * 100;
+            otherAllowanceIncreaseField.value = increase.toFixed(2);
+        } else {
+            otherAllowanceIncreaseField.value = '0';
+        }
+        updateTotals();
+    });
 
-window.onload = function() {
-    updateTotals();
-};
-
-
+    window.onload = function() {
+        updateTotals();
+    };
 </script>
