85456b6999a7d0fc8ac09a301238f52a711fd9da
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-03 15:41:32 +0800

    add employee dropdown

--- app/Http/Controllers/EmployeeTrainingController.php
+++ app/Http/Controllers/EmployeeTrainingController.php
@@ -9,63 +9,70 @@ use RealRashid\SweetAlert\Facades\Alert;
 
 class EmployeeTrainingController extends Controller
 {
-  public function index() {
+    public function index()
+    {
 
-    $employee = Employee::where('status', 'Active')->get();
+        $employee = Employee::where('status', 'Active')->get();
 
-    $employeeTraining = EmployeeTraining::get();
+        $employeeTraining = EmployeeTraining::get();
 
-    return view('hr-portal.training', array(
-        'header' => 'hrPortal',
-        'employee' => $employee,
-        'employeeTraining' => $employeeTraining
-      )
-    );
-  }
+        return view(
+            'hr-portal.training',
+            array(
+                'header' => 'hrPortal',
+                'employee' => $employee,
+                'employeeTraining' => $employeeTraining
+            )
+        );
+    }
 
-  public function store(Request $request) {
-    $employeeTraining = new EmployeeTraining;
-    $employeeTraining->employee_id = $request->user_id;
-    $employeeTraining->start_date = $request->start_date;
-    $employeeTraining->end_date = $request->end_date;
-    $employeeTraining->amount = $request->amount;
-    $employeeTraining->training = $request->training;
-    $employeeTraining->save();
+    public function store(Request $request)
+    {
+        $employeeTraining = new EmployeeTraining;
+        $employeeTraining->employee_id = $request->user_id;
+        $employeeTraining->start_date = $request->start_date;
+        $employeeTraining->end_date = $request->end_date;
+        $employeeTraining->amount = $request->amount;
+        $employeeTraining->training = $request->training;
+        $employeeTraining->save();
 
-    Alert::success('Successfully Added')->persistent('Dismiss');
-    return back();
-  }
+        Alert::success('Successfully Added')->persistent('Dismiss');
+        return back();
+    }
 
-  public function update(Request $request, $id) {
-    $employeeTraining = EmployeeTraining::findOrFail($id);
-    $employeeTraining->employee_id = $request->employee;
-    $employeeTraining->start_date = $request->start_date;
-    $employeeTraining->end_date = $request->end_date;
-    $employeeTraining->amount = $request->amount;
-    $employeeTraining->training = $request->training;
-    $employeeTraining->save();
-    
-    Alert::success('Successfully Updated')->persistent('Dismiss');
-    return back();
-  }
+    public function update(Request $request, $id)
+    {
+        $employeeTraining = EmployeeTraining::findOrFail($id);
+        $employeeTraining->employee_id = $request->employee;
+        $employeeTraining->start_date = $request->start_date;
+        $employeeTraining->end_date = $request->end_date;
+        $employeeTraining->amount = $request->amount;
+        $employeeTraining->training = $request->training;
+        $employeeTraining->save();
 
-  public function delete($id) {
-    $employeeTraining = EmployeeTraining::findOrFail($id);
-    $employeeTraining->delete();
+        Alert::success('Successfully Updated')->persistent('Dismiss');
+        return back();
+    }
 
-    Alert::success('Successfully Deleted')->persistent('Dismiss');
-    return back();
-  }
+    public function delete($id)
+    {
+        $employeeTraining = EmployeeTraining::findOrFail($id);
+        $employeeTraining->delete();
 
-  public function employeeTrainingReports() {
+        Alert::success('Successfully Deleted')->persistent('Dismiss');
+        return back();
+    }
 
-    $employeeTraining = EmployeeTraining::get();
+    public function employeeTrainingReports()
+    {
+        $employeeTraining = EmployeeTraining::where('employee_id', auth()->user()->employee->id)->get();
 
-    return view('hr_report.training',
-      array(
-        'header' => 'hrReport',
-        'employeeTraining' => $employeeTraining
-      )
-    );
-  }
+        return view(
+            'hr_report.training',
+            array(
+                'header' => 'hrReport',
+                'employeeTraining' => $employeeTraining
+            )
+        );
+    }
 }
