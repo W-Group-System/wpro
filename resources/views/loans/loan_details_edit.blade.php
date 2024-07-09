8712d2528fcd7c13593f7b0e670d8f9e35a90b28
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-07-09 09:36:31 +0800

    fix bug in loan register

--- resources/views/loans/loan_details_edit.blade.php
+++ resources/views/loans/loan_details_edit.blade.php
@@ -8,7 +8,7 @@
 				</button>
 			</div>
 			<div class="modal-body">
-                <form method='POST' action='{{url('update-loan/'.$loan->id)}}' onsubmit='show()' enctype="multipart/form-data">
+                <form method='POST' action='{{url('update-loan/'.$loan->id)}}' onsubmit='show()'>
                     @csrf
                 <div class="row">
                     <div class="col-lg-6 form-group">
@@ -25,29 +25,28 @@
                     </div>
                     <div class="col-lg-6 form-group">
                         <label for="employee">Employee</label>
-                        <select data-placeholder="Select Employee" class="form-control form-control-sm required js-example-basic-single "
-                            style='width:100%;' name='employee' required>
+                        {{-- <select data-placeholder="Select Employee" class="form-control form-control-sm required js-example-basic-single "
+                            style='width:100%;' name='employee' required readonly>
                             <option value="">--Select Employee--</option>
                             @foreach ($employees as $employee)
-                                <option value="{{ $employee->id }}" {{$loan->employee ? 'selected' : '' }}>
+                                <option value="{{ $employee->id }}" {{$loan->employee->id == $employee->id ? 'selected' : '' }}>
                                     {{ $employee->last_name . ', ' . $employee->first_name . ' ' . $employee->middle_name }}</option>
                             @endforeach
-                        </select>
+                        </select> --}}
+                        <p style="font-size: 15px;">
+                            {{ $loan->employee->last_name . ', ' . $loan->employee->first_name . ' ' . $loan->employee->middle_name }}
+                        </p>
                     </div>
-                </div>
-                <div class="row">
                     <div class="col-lg-6 form-group">
                         <label for="amount">Amount</label>
                         <input type="number" class="form-control form-control-sm" name="amount" id="amount" required min="1"
-                            value="{{ $loan->amount}}" placeholder="0.00" readonly>
+                            value="{{ $loan->amount}}" step=".01" placeholder="0.00" readonly>
                     </div>
                     <div class="col-lg-6 form-group">
                         <label for="ammortAmt">Ammortization Amount</label>
                         <input type="number" class="form-control form-control-sm" name="monthly_ammort_amt" id="monthly_ammort_amt"
-                            required min="1" placeholder="0.00" value="{{ $loan->monthly_ammort_amt}}">
+                            required min="1" placeholder="0.00" step=".01" value="{{ $loan->monthly_ammort_amt}}">
                     </div>
-                </div>
-                <div class="row">
                     <div class="col-lg-6 form-group">
                         <label for="startDate">Start Date</label>
                         <input type="date" class="form-control form-control-sm" name="start_date" id="start_date"
@@ -58,23 +57,28 @@
                         <input type="date" class="form-control form-control-sm" name="expiry_date" id="start_date"
                             value="{{ $loan->expiry_date }}">
                     </div>
-                </div>
-                <div class="row">
                     <div class="col-lg-6 form-group">
                         <label for="amount">Loan Balance</label>
                         <input type="number" class="form-control form-control-sm" name="initial_amount" id="amount" required
-                            min="1" value="{{ $loan->initial_amount }}" placeholder="0.00">
+                            min="1" value="{{ $loan->initial_amount }}" step=".01" placeholder="0.00">
                     </div>
                     <div class="col-lg-6 form-group">
                         <label for="frequency">Frequency</label>
                         <select data-placeholder="Frequency" class="form-control form-control-sm required js-example-basic-single "
-                        style='width:100%;' name='frequency' required>
-                        <option value="">--Frequency--</option>
-                        <option value='Every cut off'>Every cut off</option>
-                        <option value='This cut off'>This cut off</option>
-                        <option value='Every 1st cut off'>Every 1st cut off</option>
-                        <option value='Every 2nd cut off'>Every 2nd cut off</option>
-                    </select>
+                            style='width:100%;' name='frequency' required>
+                            <option value="">--Frequency--</option>
+                            <option value='Every cut off' {{ $loan->schedule == 'Every cut off' ? 'selected' : '' }}>Every cut off</option>
+                            <option value='This cut off' {{ $loan->schedule == 'This cut off' ? 'selected' : '' }}>This cut off</option>
+                            <option value='Every 1st cut off' {{ $loan->schedule == 'Every 1st cut off' ? 'selected' : '' }}>Every 1st cut off</option>
+                        <option value='Every 2nd cut off' {{ $loan->schedule == 'Every 2nd cut off' ? 'selected' : '' }}>Every 2nd cut off</option>
+                        </select>
+                    </div>
+                    <div class="col-lg-6 form-group">
+                        <select data-placeholder="Select status" name="status" class="form-control form-control-sm required js-example-basic-single" style="width: 100%">
+                            <option value="">--Status--</option>
+                            <option value="Active" {{ $loan->status=="Active"?'selected':'' }}>Active</option>
+                            <option value="Inactive" {{ $loan->status=="Inactive"?'selected':'' }}>Inactive</option>
+                        </select>
                     </div>
                 </div>
                 <div class="modal-footer">
