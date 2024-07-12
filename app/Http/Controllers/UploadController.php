4dfbaf219ce411479f5166e8fac7b72fa3930895
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-07-12 17:36:05 +0800

    Upload OB

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,7 +10,10 @@ use App\UploadType;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use RealRashid\SweetAlert\Facades\Alert;
-
+use Shuchkin\SimpleXLSX;
+use App\EmployeeLeave;
+use App\EmployeeOb;
+use App\EmployeeOvertime;
 class UploadController extends Controller
 {
     public function index()
@@ -33,9 +36,135 @@ class UploadController extends Controller
         ]);
         $path = $request->file('file')->getRealPath();
 
-        $data = Excel::load($path);
-        dd($data);
-        Excel::import(new UploadImport($request->type), $request->file);
+        $xlsx = SimpleXLSX::parse($path)->rows();
+        // dd($xlsx);
+        // Excel::import(new UploadImport($request->type), $request->file);
+        // dd($xlsx);
+        
+        foreach ($xlsx as $key => $row) {
+            if($key > 0)
+            {
+                if($row[0])
+                {
+                    $user_id = Employee::where('employee_code', $row[0])->pluck('user_id')->toArray();
+                    $dateFiled =date('Y-m-d',strtotime($row[2]));
+                    
+                    
+                    if ($request->type == "OB") {
+                        $employeeOb = EmployeeOb::whereIn('user_id', $user_id)
+                            ->where('applied_date', $dateFiled)
+                            ->first();
+                        // dd($row[5]);
+                        foreach ($user_id as $uid) {
+                            if (empty($employeeOb)) {
+                                $employeeOb = new EmployeeOb;
+                                $employeeOb->user_id = $uid;
+                                $employeeOb->applied_date = $dateFiled ;
+                                $date = 
+                                $employeeOb->date_from =date("Y-m-d H:i:s",strtotime(date('Y-m-d',strtotime($row[3]))." ".date('H:i:s',strtotime($row[5]))));
+                                $employeeOb->date_to = date("Y-m-d H:i:s",strtotime(date('Y-m-d',strtotime($row[4]))." ".date('H:i:s',strtotime($row[6]))));
+                                $employeeOb->approved_date = date('Y-m-d',strtotime($row[4]));
+                                $employeeOb->status = $row[10];
+                                $employeeOb->created_by = auth()->user()->id;
+                                $employeeOb->save();
+                            } else {
+                                $employeeOb->date_from =date("Y-m-d H:i:s",strtotime(date('Y-m-d',strtotime($row[3]))." ".date('H:i:s',strtotime($row[5]))));
+                                $employeeOb->date_to = date("Y-m-d H:i:s",strtotime(date('Y-m-d',strtotime($row[4]))." ".date('H:i:s',strtotime($row[6]))));
+                                $employeeOb->approved_date = date('Y-m-d',strtotime($row[4]));
+                                $employeeOb->status =  $row[10];
+                                $employeeOb->created_by = auth()->user()->id;
+                                $employeeOb->save();
+                            }
+                        }
+                    } else if ($this->type == "OT") {
+                        $employeeOt = EmployeeOvertime::whereIn('user_id', $user_id)->where('ot_date', $dateFiled)->first();
+                        foreach ($user_id as $uid) {
+                            if (empty($employeeOt)) {
+                                $employeeOt = new EmployeeOvertime;
+                                $employeeOt->user_id = $uid;
+                                $employeeOt->ot_date = $dateFiled;
+                                $employeeOt->start_time = date('Y-m-d h:i:s', strtotime($row[3]));
+                                $employeeOt->end_time = date('Y-m-d h:i:s', strtotime($row[4]));
+                                $employeeOt->approved_date = date('Y-m-d h:i:s', strtotime($row[7]));
+                                $employeeOt->status = $row[10];
+                                $employeeOt->ot_approved_hrs = $row[5];
+                                $employeeOt->break_hrs = $row[6];
+                                $employeeOt->created_by = auth()->user()->id;
+                                $employeeOt->save();
+                            } else {
+                                $employeeOt->start_time = date('Y-m-d h:i:s', strtotime($row[3]));
+                                $employeeOt->end_time = date('Y-m-d h:i:s', strtotime($row[4]));
+                                $employeeOt->approved_date = date('Y-m-d h:i:s', strtotime($row[7]));
+                                $employeeOt->status = $row[10];
+                                $employeeOt->ot_approved_hrs = $row[5];
+                                $employeeOt->break_hrs = $row[6];
+                                $employeeOt->created_by = auth()->user()->id;
+                                $employeeOt->save();
+                            }
+                        }
+                    } else if ($this->type == "VL/SL") {
+                        $types = 0;
+                        if ($row['leave_name'] == "Vacation Leave" || $row['leave_name'] == "VL") {
+                            $types = 1;
+                        }
+                        if ($row['leave_name'] == "Sick Leave" || $row['leave_name'] == 'SL') {
+                            $types = 2;
+                        }
+        
+                        $startDate = date('Y-m-d',strtotime($row[3]));
+                        $endDate = date('Y-m-d',strtotime($row[4]));;
+                        $approved_date = date('Y-m-d',strtotime($row[7]));;
+        
+                        $leaves = EmployeeLeave::whereIn('user_id', $user_id)
+                            ->where('date_from', $startDate)
+                            ->where('date_to', $endDate)
+                            ->first();
+        
+                        foreach ($user_id as $uid) {
+                            if (!empty($leaves)) {
+                                $leaves = $leaves->delete();
+                            }
+        
+                            $leaves = new EmployeeLeave;
+                            $leaves->user_id = $uid;
+                            $leaves->leave_type = $types;
+                            $leaves->date_from =$startDate;
+                            $leaves->date_to = $endDate;
+                            $leaves->approved_date = $approved_date;
+                            $leaves->status = $row[10];
+                            $leaves->created_by = auth()->user()->id;
+                            $leaves->save();
+                        }
+                    }
+                    else if ($this->type == "DTR") {
+                        $dateTime = Date::excelToDateTimeObject($row['date_time'])->format('Y-m-d H:i:s');
+                        
+                        $attendanceLogs = AttendanceLog::where('emp_code', $row['bio_number'])->where('datetime', $dateTime)->first();
+                        
+                        if (empty($attendanceLogs)) {
+                            $attendanceLogs = new AttendanceLog;
+                            $attendanceLogs->emp_code = $row['bio_number'];
+                            $attendanceLogs->date = Date::excelToDateTimeObject($row['date_time'])->format('Y-m-d');
+                            $attendanceLogs->datetime = $dateTime;
+                            $attendanceLogs->type = strtoupper($row['status']) == 'IN' ? 1 : 0;
+                            $attendanceLogs->location = "DTR";
+                            $attendanceLogs->ip_address = 'DTR Upload';
+                            $attendanceLogs->save();
+                        }
+                        else {
+                            $attendanceLogs->emp_code = $row['bio_number'];
+                            $attendanceLogs->date = Date::excelToDateTimeObject( $row['date_time'])->format('Y-m-d');
+                            $attendanceLogs->datetime = $dateTime;
+                            $attendanceLogs->type = strtoupper($row['status']) == 'IN' ? 1 : 0;
+                            $attendanceLogs->save();
+                        }
+                    }
+                }
+           
+            }
+            
+        }
+
 
         $file = $request->file('file');
         $fileName = time() . '_' . $file->getClientOriginalName();
@@ -49,6 +178,7 @@ class UploadController extends Controller
         $uploadData->type = $request->type;
         $uploadData->save();
 
+        Alert::success('Successfully Uploaded')->persistent('Dismiss');
         return back();
     }
 
