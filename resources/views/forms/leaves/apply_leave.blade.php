2bddfd99d6ae1f3c3ce2c3c5dc83281384c0d97e
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-07-23 13:19:15 +0800

    leave

--- resources/views/forms/leaves/apply_leave.blade.php
+++ resources/views/forms/leaves/apply_leave.blade.php
@@ -238,12 +238,12 @@
       } else if ($(this).val() == 2) {
         
         $("[name='date_from']").attr({
-          'min': "{{date('Y-m-d', strtotime('-1 week'))}}",
+          'min': "{{date('Y-m-d', strtotime('-3 weekdays'))}}",
           'max': "{{date('Y-m-d', strtotime('-1 days'))}}"
         });
 
         $("[name='date_to']").attr({
-          'min': "{{date('Y-m-d', strtotime('-1 week'))}}",
+          'min': "{{date('Y-m-d', strtotime('-3 weekdays'))}}",
           'max': "{{date('Y-m-d', strtotime('-1 days'))}}"
         });
       }
