0c72c939acdd514bbb6b2f34f9f8a3ac00f80458
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-08-08 15:47:30 +0800

    PayReg report

--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\PayRegs;
+use App\Company;
 use Illuminate\Http\Request;
 
 class PayrollController extends Controller
@@ -13,10 +14,102 @@ class PayrollController extends Controller
             'header' => 'reports',
         ));
     }
-    public function payroll_report()
+    public function payroll_report(Request $request)
     {
+        $allowed_companies = getUserAllowedCompanies(auth()->user()->id);
+        $companies = Company::whereHas('employee_has_company')
+        ->whereIn('id', $allowed_companies)
+        ->get();
+        $company = $request->company;
+        $pay_registers = Payregs::selectRaw('
+        employee_no,
+        last_name,
+        first_name,
+        middle_name,
+        department,
+        account_number,
+        SUM(days_rendered) as days_rendered,
+        SUM(basic_pay) as basic_pay,
+        SUM(other_allowances_basic_pay) as other_allowances_basic_pay,
+        SUM(subliq) as subliq,
+        SUM(sh_nd) as sh_nd,
+        SUM(sh_amount) as sh_amount,
+        SUM(sh_ge) as sh_ge,
+        SUM(sh_nd_ge) as sh_nd_ge,
+        SUM(sh_nd_ge_amount) as sh_nd_ge_amount,
+        SUM(sh_ot) as sh_ot,
+        SUM(sh_ot_amount) as sh_ot_amount,
+        SUM(sh_ot_ge) as sh_ot_ge,
+        SUM(sh_ot_ge_amount) as sh_ot_ge_amount,
+        SUM(sh_nd_amount) as sh_nd_amount,
+        SUM(lh_nd) as lh_nd,
+        SUM(lh_nd_amount) as lh_nd_amount,
+        SUM(lh_nd_ge) as lh_nd_ge,
+        SUM(lh_nd_ge_amount) as lh_nd_ge_amount,
+        SUM(lh_ot) as lh_ot,
+        SUM(lh_ot_amount) as lh_ot_amount,
+        SUM(lh_ot_ge) as lh_ot_ge,
+        SUM(lh_ot_ge_amount) as lh_ot_ge_amount,
+        SUM(reg_nd) as reg_nd,
+        SUM(reg_nd_amount) as reg_nd_amount,
+        SUM(reg_ot) as reg_ot,
+        SUM(reg_ot_amount) as reg_ot_amount,
+        SUM(reg_ot_nd) as reg_ot_nd,
+        SUM(reg_ot_nd_amount) as reg_ot_nd_amount,
+        SUM(rst_nd) as rst_nd,
+        SUM(rst_nd_amount) as rst_nd_amount,
+        SUM(rst_nd_ge) as rst_nd_ge,
+        SUM(rst_nd_ge_amount) as rst_nd_ge_amount,
+        SUM(rst_ot) as rst_ot,
+        SUM(rst_ot_amount) as rst_ot_amount,
+        SUM(rst_ot_ge) as rst_ot_ge,
+        SUM(rst_ot_ge_amount) as rst_ot_ge_amount,
+        SUM(ot_total) as ot_total,
+        SUM(pl) as pl,
+        SUM(pl_amount) as pl_amount,
+        SUM(sl) as sl,
+        SUM(sl_amount) as sl_amount,
+        SUM(vl) as vl,
+        SUM(vl_amount) as vl_amount,
+        SUM(leave_amount_total) as leave_amount_total,
+        SUM(salary_adjustment) as salary_adjustment,
+        SUM(taxable_benefits_total) as taxable_benefits_total,
+        SUM(gross_taxable_income) as gross_taxable_income,
+        SUM(days_absent) as days_absent,
+        SUM(absent_amount) as absent_amount,
+        SUM(tardiness_total) as tardiness_total,
+        SUM(tardiness_amount) as tardiness_amount,
+        SUM(undertime_total) as undertime_total,
+        SUM(undertime_amount) as undertime_amount,
+        SUM(sss_ec) as sss_ec,
+        SUM(sss_employee_share) as sss_employee_share,
+        SUM(sss_employer_share) as sss_employer_share,
+        SUM(hdmf_employee_share) as hdmf_employee_share,
+        SUM(hdmf_employer_share) as hdmf_employer_share,
+        SUM(phic_employee_share) as phic_employee_share,
+        SUM(phic_employer_share) as phic_employer_share,
+        SUM(mpf_employee_share) as mpf_employee_share,
+        SUM(mpf_employer_share) as mpf_employer_share,
+        SUM(statutory_total) as statutory_total,
+        SUM(taxable_deductible_total) as taxable_deductible_total,
+        SUM(net_taxable_income) as net_taxable_income,
+        SUM(withholding_tax) as withholding_tax,
+        SUM(deminimis) as deminimis,
+        SUM(nontaxable_benefits_total) as nontaxable_benefits_total,
+        SUM(nontaxable_deductible_benefits_total) as nontaxable_deductible_benefits_total,
+        SUM(gross_pay) as gross_pay,
+        SUM(deductions_total) as deductions_total,
+        SUM(netpay) as netpay
+    ')
+    ->whereBetween('pay_period_from', [$request->from, $request->to])
+    ->where('company_id', $request->company)
+    ->groupBy('employee_no', 'last_name', 'first_name', 'middle_name', 'department', 'account_number')
+    ->get();
         return view('reports.payroll_report', array(
             'header' => 'reports',
+            'companies' => $companies,
+            'company' => $company,
+            'pay_registers' => $pay_registers,
         ));
     }
 }
--- resources/views/reports/payroll_report.blade.php
+++ resources/views/reports/payroll_report.blade.php
@@ -10,7 +10,17 @@
 						<p class="card-description">
 						<form method='get' onsubmit='show();' enctype="multipart/form-data">
 							<div class=row>
-								<div class='col-md-4'>
+								<div class="col-md-3">
+                                    <div class="form-group">
+                                        <select data-placeholder="Select Company" onchange='clear();' class="form-control form-control-sm required js-example-basic-single" style="width:100%;" name="company" id="companySelect" required>
+                                            <option value="">-- Select Company --</option>
+                                            @foreach($companies as $comp)
+                                            <option value="{{$comp->id}}" @if ($comp->id == $company) selected @endif>{{$comp->company_code}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+								<div class='col-md-3'>
 									<div class="form-group row">
 										<label class="col-sm-4 col-form-label text-right">From</label>
 										<div class="col-sm-8">
@@ -19,7 +29,7 @@
 										</div>
 									</div>
 								</div>
-								<div class='col-md-4'>
+								<div class='col-md-3'>
 									<div class="form-group row">
 										<label class="col-sm-4 col-form-label text-right">To</label>
 										<div class="col-sm-8">
@@ -28,7 +38,7 @@
 										</div>
 									</div>
 								</div>
-								<div class='col-md-4'>
+								<div class='col-md-3'>
 									<button type="submit" class="form-control form-control-sm btn btn-primary mb-2 btn-sm">Generate</button>
 								</div>
 							</div>
@@ -37,6 +47,232 @@
 					</div>
 				</div>
 			</div>
+			<div class="table-responsive">
+				<table class="table table-db table-hover table-bordered">
+				  <thead>
+					  <tr>
+						  <th>Employee No</th>
+						  <th>Last Name</th>
+						  <th>First Name</th>
+						  <th>Middle Name</th>
+						  <th>Department</th>
+						  <th>Cost Center</th>
+						  <th>Account No</th>
+						  <th>Tax Status</th>
+						  <th>DAYS RENDERED</th>
+						  <th>BASIC PAY</th>
+						  <th>LH ND</th>
+						  <th>LH ND AMOUNT</th>
+						  <th>LH ND GE</th>
+						  <th>LH ND GE AMOUNT</th>
+						  <th>LH OT</th>
+						  <th>LH OT Amount</th>
+						  <th>LH OT OVER 8</th>
+						  <th>LH OT OVER 8 AMOUNT</th>
+						  <th>REG ND</th>
+						  <th>REG ND AMOUNT</th>
+						  <th>REG OT</th>
+						  <th>REG OT AMOUNT</th>
+						  <th>REG OT ND</th>
+						  <th>REG OT ND AMOUNT</th>
+						  <th>RST ND</th>
+						  <th>RST ND AMOUNT</th>
+						  <th>RST ND GE</th>
+						  <th>RST ND GE AMOUNT</th>
+						  <th>RST OT</th>
+						  <th>RST OT AMOUNT</th>
+						  <th>RST OT OVER 8</th>
+						  <th>RST OT OVER 8 AMOUNT</th>
+						  <th>OVERTIME TOTAL</th>
+						  {{-- <th>PL</th>
+						  <th>PL AMOUNT</th>
+						  <th>SL</th>
+						  <th>SL AMOUNT</th>
+						  <th>VL</th>
+						  <th>VL AMOUNT</th>
+						  <th>LEAVE AMOUNT TOTAL</th> --}}
+						  <th>SALARY ADJUSTMENT</th>
+						  <th>TAXABLE BENEFITS TOTAL</th>
+						  <th>GROSS TAXABLE INCOME</th>
+						  <th>DAYS ABSENT</th>
+						  <th>ABSENT AMOUNT</th>
+						  <th>TARDINESS TOTAL</th>
+						  <th>TARDINESS AMOUNT</th>
+						  <th>UNDERTIME TOTAL</th>
+						  <th>UNDERTIME AMOUNT</th>
+						  <th>SSS EC</th>
+						  <th>SSS EMPLOYEE SHARE</th>
+						  <th>SSS EMPLOYER SHARE</th>
+						  <th>HDMF EMPLOYEE SHARE</th>
+						  <th>HDMF EMPLOYER SHARE</th>
+						  <th>PHIC EMPLOYEE SHARE</th>
+						  <th>PHIC EMPLOYER SHARE</th>
+						  <th>MPF EMPLOYEE SHARE</th>
+						  <th>MPF EMPLOYER SHARE</th>
+						  <th>STATUTORY TOTAL</th>
+						  <th>TAXABLE DEDUCTIBLE TOTAL</th>
+						  <th>NET TAXABLE INCOME</th>
+						  <th>WITHHOLDING TAX</th>
+						  <th>DEMINIMIS</th>
+						  <th>Other Allowances</th>
+						  <th>SUBLIQ</th>
+						  {{-- <th>DEMINIMIS ADJUSTMENT</th>
+						  <th>LOAD ALLOWANCE</th>
+						  <th>OTHER ALLOWANCES</th>
+						  <th>OTHER NTA</th>
+						  <th>SSS LOAN REFUND</th>
+						  <th>SUBLIQ</th> --}}
+						  {{-- @foreach($allowances_total as $total_allow)
+						  <th>{{$total_allow->allowance->name}}</th>
+						  @endforeach --}}
+						  {{-- <th>OTHER ALLOWANCES</th> --}}
+						  <th>NONTAXABLE BENEFITS TOTAL</th>
+						  
+						  {{-- <th>CANTEEN</th>
+						  <th>EMERGENCY</th>
+						  <th>HDMF CALAMITY LOAN</th>
+						  <th>HDMF CONTRIBUTION UPGRADE</th>
+						  <th>HDMF LOAN</th>
+						  <th>MOTORCYCLE LOAN</th>
+						  <th>RICE LOAN</th>
+						  <th>SSS CALAMITY LOAN</th>
+						  <th>SSS LOAN</th>
+						  <th>STAFF HOUSE</th>
+						  <th>WESLA LOAN</th> --}}
+						  {{-- @foreach($instructions as $ins)
+						  <th>{{$ins->benefit_name}}</th>
+						  @endforeach --}}
+						  {{-- <th>Others</th> --}}
+						  {{-- @foreach($loans_all as $loans_al)
+						  <th>{{$loans_al->loan_type->loan_name}}</th>
+						  @endforeach --}}
+						  <th>NONTAXABLE DEDUCTIBLE BENEFITS TOTAL</th>
+						  <th>GROSS PAY</th>
+						  <th>DEDUCTIONS TOTAL</th>
+						  <th>NETPAY</th>
+					  </tr>
+				  </thead>
+				  <tbody>
+					@foreach($pay_registers as $key => $pay_reg)
+					<tr>
+						<td>{{$pay_reg->employee_no}}</td>
+						<td>{{$pay_reg->last_name}}</td>
+						<td>{{$pay_reg->first_name}}</td>
+						<td>{{$pay_reg->middle_name}}</td>
+						<td>{{$pay_reg->department}}</td>
+						<td>{{$pay_reg->cost_center}}</td>
+						<td>{{$pay_reg->account_number}}</td>
+						<td>{{$pay_reg->tax_status}}</td>
+						<td>{{$pay_reg->days_rendered}}</td>
+						<td>{{$pay_reg->basic_pay}}</td>
+						<td>{{$pay_reg->lh_nd}}</td>
+						<td>{{$pay_reg->lh_nd_amount}}</td>
+						<td>{{$pay_reg->lh_nd_ge}}</td>
+						<td>{{$pay_reg->lh_nd_ge_amount}}</td>
+						<td>{{$pay_reg->lh_ot}}</td>
+						<td>{{$pay_reg->lh_ot_amount}}</td>
+						<td>{{$pay_reg->lh_ot_ge}}</td>
+						<td>{{$pay_reg->lh_ot_ge_amount}}</td>
+						<td>{{$pay_reg->reg_nd}}</td>
+						<td>{{$pay_reg->reg_nd_amount}}</td>
+						<td>{{$pay_reg->reg_ot}}</td>
+						<td>{{$pay_reg->reg_ot_amount}}</td>
+						<td>{{$pay_reg->reg_ot_nd}}</td>
+						<td>{{$pay_reg->reg_ot_nd_amount}}</td>
+						<td>{{$pay_reg->rst_nd}}</td>
+						<td>{{$pay_reg->rst_nd_amount}}</td>
+						<td>{{$pay_reg->rst_nd_ge}}</td>
+						<td>{{$pay_reg->rst_nd_ge_amount}}</td>
+						<td>{{$pay_reg->rst_ot}}</td>
+						<td>{{$pay_reg->rst_ot_amount}}</td>
+						<td>{{$pay_reg->rst_ot_ge}}</td>
+						<td>{{$pay_reg->rst_ot_ge_amount}}</td>
+						<td>{{$pay_reg->ot_total}}</td>
+						<td>{{$pay_reg->salary_adjustment}}</td>
+						<td>{{$pay_reg->taxable_benefits_total}}</td>
+						<td>{{$pay_reg->gross_taxable_income}}</td>
+						<td>{{$pay_reg->days_absent}}</td>
+						<td>{{$pay_reg->absent_amount}}</td>
+						<td>{{$pay_reg->tardiness_total}}</td>
+						<td>{{$pay_reg->tardiness_amount}}</td>
+						<td>{{$pay_reg->undertime_total}}</td>
+						<td>{{$pay_reg->undertime_amount}}</td>
+						<td>{{$pay_reg->sss_ec}}</td>
+						<td>{{$pay_reg->sss_employee_share}}</td>
+						<td>{{$pay_reg->sss_employer_share}}</td>
+						<td>{{$pay_reg->hdmf_employee_share}}</td>
+						<td>{{$pay_reg->hdmf_employer_share}}</td>
+						<td>{{$pay_reg->phic_employee_share}}</td>
+						<td>{{$pay_reg->phic_employer_share}}</td>
+						<td>{{$pay_reg->mpf_employee_share}}</td>
+						<td>{{$pay_reg->mpf_employer_share}}</td>
+						<td>{{$pay_reg->statutory_total}}</td>
+						<td>{{$pay_reg->taxable_deductible_total}}</td>
+						<td>{{$pay_reg->net_taxable_income}}</td>
+						<td>{{$pay_reg->withholding_tax}}</td>
+						<td>{{$pay_reg->deminimis}}</td>
+						<td>{{$pay_reg->other_allowances_basic_pay}}</td>
+						<td>{{$pay_reg->subliq}}</td>
+						<td>{{$pay_reg->nontaxable_benefits_total}}</td>
+						<td>{{$pay_reg->nontaxable_deductible_benefits_total}}</td>
+						<td>{{$pay_reg->gross_pay}}</td>
+						<td>{{$pay_reg->deductions_total}}</td>
+						<td>{{$pay_reg->netpay}}</td>
+					</tr>
+					@endforeach
+				  </tbody>
+				</table>
+			  </div>
 		</div>
 	</div>
+
+	<link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.2/css/buttons.dataTables.css">
+
+<script src="https://cdn.datatables.net/2.0.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.dataTables.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+<script>
+    $(document).ready(function() {
+    new DataTable('.table-db', {
+      // pagelenth:25,
+      paginate:false,
+      dom: 'Bfrtip',
+      buttons: [
+          'copy', 'excel'
+      ],
+      columnDefs: [{
+        "defaultContent": "-",
+        "targets": "_all"
+      }],
+      order: [] 
+    });
+  });
+
+  
+  function showConfirmation() {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Do you want to submit this Pay Reg?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, submit it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                document.getElementById("loader").style.display = "flex";
+                document.getElementById('PagRegForm').submit();
+            }
+        });
+
+        return false; // Prevent default form submission
+    }
+</script>
+    {{-- @foreach($payrolls as $payroll)
+        @include('payroll.view_payroll')   
+    @endforeach
+    @include('payroll.upload_payroll') --}}
 @endsection
