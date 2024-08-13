16617b6f89635d6c1520ecfdde9412ec7ad36e38
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-08-13 19:11:51 +0800

    Done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
          if(env('FORCE_HTTPS',false)) { // Default value should be false for local server\
              URL::forceScheme('https');
          }
-         
-        URL::forceScheme('https');
+         \URL::forceScheme('https');
+        // URL::forceScheme('https');
 
        
     }
