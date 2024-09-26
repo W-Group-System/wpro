5054f03cdc483eabf22bcd35dae81316aa589e75
Schultz Henry Boado <schultzxhenry@gmail.com>
2024-09-27 07:59:19 +0800

    get resign employee for recruitment-portal

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2622,4 +2622,11 @@ class EmployeeController extends Controller
             )
         );
     }
+    
+    public function resigned()
+    {
+        $employee = Employee::where('status', 'Resigned')->get();
+
+        return response()->json($employee);
+    }
 }
