bbd69ef21c15ac50cf6a07a2d5adc93cd2cdad59
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-11-06 16:06:10 +0800

    same day leave

--- app/Http/Controllers/EmployeeLeaveController.php
+++ app/Http/Controllers/EmployeeLeaveController.php
@@ -127,8 +127,7 @@ class EmployeeLeaveController extends Controller
         $employee = Employee::where('user_id',Auth::user()->id)->first();
         $count_days = get_count_days_leave($employee->ScheduleData,$request->date_from,$request->date_to);
         if($request->withpay == 'on'){
-            $existing_date_leave = EmployeeLeave::where('leave_type', $request->leave_type)
-                ->where(function($q)use($request) {
+            $existing_date_leave = EmployeeLeave::where(function($q)use($request) {
                     // $q->where('date_from', $request->date_from)->orWhere('date_to', $request->date_to);
                     $q->whereBetween('date_from', [$request->date_from, $request->date_to])
                         ->orWhereBetween('date_to',[$request->date_from, $request->date_to]);
@@ -182,8 +181,7 @@ class EmployeeLeaveController extends Controller
                 return back();
             }
         }else{
-            $existing_date_leave = EmployeeLeave::where('leave_type', $request->leave_type)
-                ->where(function($q)use($request) {
+            $existing_date_leave = EmployeeLeave::where(function($q)use($request) {
                     // $q->where('date_from', $request->date_from)->orWhere('date_to', $request->date_to);
                     $q->whereBetween('date_from', [$request->date_from, $request->date_to])
                         ->orWhereBetween('date_to',[$request->date_from, $request->date_to]);
