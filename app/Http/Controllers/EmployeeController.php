b7378b05c848278199a329a1a073048c1bc27466
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-11-13 10:33:10 +0800

    Doone

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1808,7 +1808,7 @@ class EmployeeController extends Controller
         $employees = [];
         
         if ($from_date != null) {
-            $emp_data = Employee::select('employee_number','user_id','first_name','last_name','middle_name','location','schedule_id','employee_code','company_id','work_description')
+            $emp_data = Employee::select('employee_number','user_id','first_name','last_name','middle_name','location','schedule_id','employee_code','company_id','work_description','original_date_hired')
                                 ->with('company')
                                 ->with(['attendances' => function ($query) use ($date_from, $to_date) {
                                     $query->whereBetween('time_in', [$date_from." 00:00:01", $to_date." 23:59:59"])
