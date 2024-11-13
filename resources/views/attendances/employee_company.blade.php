b07c256f9c6c50701f366f3b79b3d0fe46f59756
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-11-13 08:12:30 +0800

    2 holidays

--- resources/views/attendances/employee_company.blade.php
+++ resources/views/attendances/employee_company.blade.php
@@ -386,9 +386,10 @@
                                                                
                                                      
                                                                 $employee_schedule_before = employeeSchedule($schedules,date('Y-m-d',strtotime("-1 day",strtotime($date_r))),$emp->schedule_id, $emp->employee_code);
-                                                                $check_if_holiday_before = checkIfHoliday(date('Y-m-d'),date('Y-m-d',strtotime("-1 day",strtotime($date_r))),$emp->location);
+                                                                $check_if_holiday_before = checkIfHoliday(date('Y-m-d',strtotime("-1 day",strtotime($date_r))),$emp->location);
                                                                 if($check_if_holiday_before)
                                                                 {
+                                                                    dd('renz');
                                                                     $abs = 0;
                                                                 }
                                                                 if($employee_schedule_before == null)
