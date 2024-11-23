0ba5003754a3c774aeea54ea228f7f59672801f8
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-11-23 17:56:32 +0800

    For testing

--- resources/views/payroll/pay_reg.blade.php
+++ resources/views/payroll/pay_reg.blade.php
@@ -458,7 +458,7 @@
                               if($payroll_b)
                               {
                                 $last_c = $last_cut_off->where('employee_no',$name->employee_no)->where('cut_off_date','>',date('Y-m-d', strtotime($name->cut_off_date . ' -17 days')))->first();
-                                dd($last_cut_off." - ".date('Y-m-d', strtotime($name->cut_off_date . ' -17 days')));
+                                dd($name->cut_off_date." - ".date('Y-m-d', strtotime($name->cut_off_date . ' -17 days')));
                                 if($last_c)
                                 {
                                  
