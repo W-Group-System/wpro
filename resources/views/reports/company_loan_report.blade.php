3091af651fb5b649e9e8e421132afb4b65e988ed
Renz Christian Munoz. Cabato <mcabato@wlandmark.com.ph>
2024-11-29 10:46:02 +0800

    Done

--- resources/views/reports/company_loan_report.blade.php
+++ resources/views/reports/company_loan_report.blade.php
@@ -100,10 +100,10 @@
                                         <td>{{ $loan_a->employee->company->company_code }}</td>
                                         <td>{{ $loan_a->loan_type->loan_name }}</td>
                                         <td>{{ $loan_a->employee->last_name }},{{ $loan_a->employee->first_name }}</td>
-                                        <td>{{ $loan_a->amount }}</td>
-                                        <td>{{ $loan_a->monthly_ammort_amt }} </td>
-                                        <td><a href="#view{{$loan_a->id}}" data-toggle="modal" title='View'>{{ ($loan_a->pay)->sum('amount') }}</td>
-                                        <td>{{ $loan_a->initial_amount-($loan_a->pay)->sum('amount') }}</td>
+                                        <td>{{ number_format($loan_a->amount,2) }}</td>
+                                        <td>{{ number_format($loan_a->monthly_ammort_amt,2) }} </td>
+                                        <td><a href="#view{{$loan_a->id}}" data-toggle="modal" title='View'>{{ number_format(($loan_a->pay)->sum('amount'),2) }}</td>
+                                        <td>{{ number_format($loan_a->initial_amount-($loan_a->pay)->sum('amount'),2) }}</td>
                                         @if(($loan_a->pay->sortByDesc('id'))->first() != null)
                                         <td>{{$loan_a->pay->sortByDesc('id')->first()->pay_reg->pay_period_from}} - {{$loan_a->pay->sortByDesc('id')->first()->pay_reg->pay_period_to}}</td>
                                         @else
