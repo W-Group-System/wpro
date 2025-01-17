94fec4dbe7dfb851b9c421314e73a21d9ad8d8f5
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-01-17 16:08:26 +0800

    rename

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -674,7 +674,7 @@
                     <li class="nav-item"> <a class="nav-link" href="{{ url('/totalExpense-report') }}">Total Expenses</a></li>
                     
                     @if (checkUserPrivilege('payroll_view',auth()->user()->id) == 'yes')
-                    <li class="nav-item"> <a class="nav-link" href="{{ url('/loan-report') }}">Loans Report</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="{{ url('/loan-report') }}">Deduction Report</a></li>
                     {{-- <li class="nav-item"> <a class="nav-link" href="{{ url('/company-loan-report') }}">Company Loan Report</a></li> --}}
                     <li class="nav-item"> <a class="nav-link" href="{{ url('/government-report') }}">Government Reports</a></li>
                     <li class="nav-item"> <a class="nav-link" href="{{ url('/payroll-report') }}">Payroll Reports</a></li>
