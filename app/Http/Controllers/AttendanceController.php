2dbc2759139550392969eb100427cd06d8e4eaf5
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-04-14 11:49:54 +0800

    remove last id

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -842,7 +842,7 @@ class AttendanceController extends Controller
                             $attendance->employee_code  = $att->emp_code;   
                             $attendance->time_in = date('Y-m-d H:i:s',strtotime($att->datetime));
                             $attendance->device_in = $att->location ." - ".$att->ip_address;
-                            $attendance->last_id = $att->id;
+                            // $attendance->last_id = $att->id;
                             $attendance->save();
                         }
                     }
@@ -854,7 +854,7 @@ class AttendanceController extends Controller
                         $update = [
                             'time_out' =>  date('Y-m-d H:i:s', strtotime($att->datetime)),
                             'device_out' => $att->location ." - ".$att->ip_address,
-                            'last_id' =>$att->id,
+                            // 'last_id' =>$att->id,
                         ];
                     
                         $attendance_in = Attendance::where('employee_code',$att->emp_code)
@@ -871,7 +871,7 @@ class AttendanceController extends Controller
                             $attendance->employee_code  = $att->emp_code;   
                             $attendance->time_out = date('Y-m-d H:i:s', strtotime($att->datetime));
                             $attendance->device_out = $att->location ." - ".$att->ip_address;
-                            $attendance->last_id = $att->id;
+                            // $attendance->last_id = $att->id;
                             $attendance->save(); 
                         }
                     }
