6400a6e94f19a09d3e09a3a7e584f7e34c10d24d
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-05-29 18:09:44 +0800

    done

--- resources/views/attendances/employee_attendance.blade.php
+++ resources/views/attendances/employee_attendance.blade.php
@@ -161,9 +161,9 @@
                                             value="{{ !empty($date_r) ? date('Y-m-d', strtotime($date_r)) : '' }}">
                                             {{ !empty($date_r) ? date('d/m/Y', strtotime($date_r)) : 'N/A' }}
                                     </td>
-                                    <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][shift]" value="{{$employee_schedule && $employee_schedule->time_in_from != '00:00' ? date('h:i A', strtotime($employee_schedule->time_in_from)) . '-' . date('h:i A', strtotime($employee_schedule->time_out_to)) : 'RESTDAY'}}">
+                                    <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][shift]" value="{{$employee_schedule && $employee_schedule->time_in_to != null ? date('h:i A', strtotime($employee_schedule->time_in_to)) . '-' . date('h:i A', strtotime($employee_schedule->time_out_to)) : 'RESTDAY'}}">
                                         @if($employee_schedule != null)
-                                            @if($employee_schedule->time_in_from != '00:00')
+                                            @if($employee_schedule->time_in_from)
                                             <small>{{date('h:i A', strtotime($employee_schedule->time_in_to)).'-'.date('h:i A', strtotime($employee_schedule->time_out_to))}}</small>
                                             @if ($employee_schedule->time_in_from != $employee_schedule->time_in_to)
                                                 <small>(Flexi)</small>
