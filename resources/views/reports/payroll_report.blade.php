ee9b981aceb68e8278aa216b8fa655b9e618a592
Mark Ian <bautistamarkian083@gmail.com>
2025-06-19 18:08:01 +0800

    add RST LH column in payroll reports

--- resources/views/reports/payroll_report.blade.php
+++ resources/views/reports/payroll_report.blade.php
@@ -44,241 +44,259 @@
 							</div>
 						</form>
 						</p>
+						<div class="table-responsive">
+						<table class="table table-db table-hover table-bordered" width="100%">
+							<thead>
+								<tr>
+									<th>Employee No</th>
+									<th>Last Name</th>
+									<th>First Name</th>
+									<th>Middle Name</th>
+									<th>Cost Center</th>
+									<th>Department</th>
+									<th>Account No</th>
+									<th>Tax Status</th>
+									<th>DAYS RENDERED</th>
+									<th>BASIC PAY</th>
+									<th>LH ND</th>
+									<th>LH ND AMOUNT</th>
+									<th>LH ND GE</th>
+									<th>LH ND GE AMOUNT</th>
+									<th>LH OT</th>
+									<th>LH OT Amount</th>
+									<th>LH OT OVER 8</th>
+									<th>LH OT OVER 8 AMOUNT</th>
+									<th>REG ND</th>
+									<th>REG ND AMOUNT</th>
+									<th>REG OT</th>
+									<th>REG OT AMOUNT</th>
+									<th>REG OT ND</th>
+									<th>REG OT ND AMOUNT</th>
+									<th>RST ND</th>
+									<th>RST ND AMOUNT</th>
+									<th>RST ND GE</th>
+									<th>RST ND GE AMOUNT</th>
+									<th>RST OT</th>
+									<th>RST OT AMOUNT</th>
+									<th>RST OT OVER 8</th>
+									<th>RST OT OVER 8 AMOUNT</th>
+									<th>RST LH OT</th>
+									<th>RST LH OT AMOUNT</th>
+									<th>RST LH OT OVER 8</th>
+									<th>RST LH OT OVER 8 AMOUNT</th>
+									<th>RST LH OT ND</th>
+									<th>RST LH OT ND AMOUNT</th>
+									<th>RST LH OT ND OVER 8</th>
+									<th>RST LH OT ND OVER 8 AMOUNT</th>
+									<th>OVERTIME TOTAL</th>
+									{{-- <th>PL</th>
+									<th>PL AMOUNT</th>
+									<th>SL</th>
+									<th>SL AMOUNT</th>
+									<th>VL</th>
+									<th>VL AMOUNT</th>
+									<th>LEAVE AMOUNT TOTAL</th> --}}
+									@foreach($salary_adjustments as $salary_adjustment)
+									<th>{{$salary_adjustment->name}}</th>
+									@endforeach
+									<th>Total SALARY ADJUSTMENT</th>
+									<th>TAXABLE BENEFITS TOTAL</th>
+									<th>GROSS TAXABLE INCOME</th>
+									<th>DAYS ABSENT</th>
+									<th>ABSENT AMOUNT</th>
+									<th>TARDINESS TOTAL</th>
+									<th>TARDINESS AMOUNT</th>
+									<th>UNDERTIME TOTAL</th>
+									<th>UNDERTIME AMOUNT</th>
+									<th>SSS EC</th>
+									<th>SSS EMPLOYEE SHARE</th>
+									<th>SSS EMPLOYER SHARE</th>
+									<th>HDMF EMPLOYEE SHARE</th>
+									<th>HDMF EMPLOYER SHARE</th>
+									<th>PHIC EMPLOYEE SHARE</th>
+									<th>PHIC EMPLOYER SHARE</th>
+									<th>MPF EMPLOYEE SHARE</th>
+									<th>MPF EMPLOYER SHARE</th>
+									<th>STATUTORY TOTAL</th>
+									<th>TAXABLE DEDUCTIBLE TOTAL</th>
+									<th>NET TAXABLE INCOME</th>
+									<th>WITHHOLDING TAX</th>
+									<th>DEMINIMIS</th>
+									<th>Other Allowances</th>
+									<th>SUBLIQ</th>
+									{{-- <th>DEMINIMIS ADJUSTMENT</th>
+									<th>LOAD ALLOWANCE</th>
+									<th>OTHER ALLOWANCES</th>
+									<th>OTHER NTA</th>
+									<th>SSS LOAN REFUND</th>
+									<th>SUBLIQ</th> --}}
+									@foreach($allowances as $total_allow)
+									<th>{{$total_allow->allowance_type->name}}</th>
+									@endforeach
+									{{-- <th>OTHER ALLOWANCES</th> --}}
+									<th>NONTAXABLE BENEFITS TOTAL</th>
+									
+									{{-- <th>CANTEEN</th>
+									<th>EMERGENCY</th>
+									<th>HDMF CALAMITY LOAN</th>
+									<th>HDMF CONTRIBUTION UPGRADE</th>
+									<th>HDMF LOAN</th>
+									<th>MOTORCYCLE LOAN</th>
+									<th>RICE LOAN</th>
+									<th>SSS CALAMITY LOAN</th>
+									<th>SSS LOAN</th>
+									<th>STAFF HOUSE</th>
+									<th>WESLA LOAN</th> --}}
+									@foreach($instructions as $ins)
+									<th>{{$ins->instruction_name}}</th>
+									@endforeach
+									{{-- <th>Others</th> --}}
+									@foreach($loans as $loans_al)
+									<th>{{$loans_al->loan_type->loan_name}}</th>
+									@endforeach
+									<th>NONTAXABLE DEDUCTIBLE BENEFITS TOTAL</th>
+									<th>GROSS PAY</th>
+									<th>DEDUCTIONS TOTAL</th>
+									<th>NETPAY</th>
+								</tr>
+							</thead>
+							<tbody>
+								@foreach($pay_registers as $key => $pay_reg)
+								<tr>
+									<td>{{$pay_reg->employee_no}}</td>
+									<td>{{$pay_reg->employee->last_name}}</td>
+									<td>{{$pay_reg->employee->first_name}}</td>
+									<td>{{$pay_reg->employee->middle_name}}</td>
+									<td>{{$pay_reg->cost_center}}</td>
+									<td>{{$pay_reg->employee->department->name}}</td>
+									<td>{{$pay_reg->employee->account_number}}</td>
+									<td>{{$pay_reg->tax_status}}</td>
+									<td>{{$pay_reg->days_rendered}}</td>
+									<td>{{$pay_reg->basic_pay}}</td>
+									<td>{{$pay_reg->lh_nd}}</td>
+									<td>{{$pay_reg->lh_nd_amount}}</td>
+									<td>{{$pay_reg->lh_nd_ge}}</td>
+									<td>{{$pay_reg->lh_nd_ge_amount}}</td>
+									<td>{{$pay_reg->lh_ot}}</td>
+									<td>{{$pay_reg->lh_ot_amount}}</td>
+									<td>{{$pay_reg->lh_ot_ge}}</td>
+									<td>{{$pay_reg->lh_ot_ge_amount}}</td>
+									<td>{{$pay_reg->reg_nd}}</td>
+									<td>{{$pay_reg->reg_nd_amount}}</td>
+									<td>{{$pay_reg->reg_ot}}</td>
+									<td>{{$pay_reg->reg_ot_amount}}</td>
+									<td>{{$pay_reg->reg_ot_nd}}</td>
+									<td>{{$pay_reg->reg_ot_nd_amount}}</td>
+									<td>{{$pay_reg->rst_nd}}</td>
+									<td>{{$pay_reg->rst_nd_amount}}</td>
+									<td>{{$pay_reg->rst_nd_ge}}</td>
+									<td>{{$pay_reg->rst_nd_ge_amount}}</td>
+									<td>{{$pay_reg->rst_ot}}</td>
+									<td>{{$pay_reg->rst_ot_amount}}</td>
+									<td>{{$pay_reg->rst_ot_ge}}</td>
+									<td>{{$pay_reg->rst_ot_ge_amount}}</td>
+									<td>{{$pay_reg->rst_lh_ot}}</td>
+									<td>{{$pay_reg->rst_lh_ot_amount}}</td>
+									<td>{{$pay_reg->rst_lh_ge_ot}}</td>
+									<td>{{$pay_reg->rst_lh_ge_ot_amount}}</td>
+									<td>{{$pay_reg->rst_lh_nd}}</td>
+									<td>{{$pay_reg->rst_lh_nd_amount}}</td>
+									<td>{{$pay_reg->rst_lh_ge_nd}}</td>
+									<td>{{$pay_reg->rst_lh_ge_nd_amount}}</td>
+									<td>{{$pay_reg->ot_total}}</td>
+									@foreach($salary_adjustments as $as => $salary_adjustment)
+										@php
+											$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
+											$salaryaddjustment_all = 0;
+											foreach($ids as $idffff)
+											{
+												$alll = ($idffff->salary_adjustments_data)->where('name',$salary_adjustment->name)->sum('amount');
+												$salaryaddjustment_all = $salaryaddjustment_all + $alll;
+											}
+											
+										@endphp
+									<td>{{number_format($salaryaddjustment_all,2)}}</td>
+									@endforeach
+									<td>{{$pay_reg->salary_adjustment}}</td>
+									<td>{{$pay_reg->taxable_benefits_total}}</td>
+									<td>{{$pay_reg->gross_taxable_income}}</td>
+									<td>{{$pay_reg->days_absent}}</td>
+									<td>{{$pay_reg->absent_amount}}</td>
+									<td>{{$pay_reg->tardiness_total}}</td>
+									<td>{{$pay_reg->tardiness_amount}}</td>
+									<td>{{$pay_reg->undertime_total}}</td>
+									<td>{{$pay_reg->undertime_amount}}</td>
+									<td>{{$pay_reg->sss_ec}}</td>
+									<td>{{$pay_reg->sss_employee_share}}</td>
+									<td>{{$pay_reg->sss_employer_share}}</td>
+									<td>{{$pay_reg->hdmf_employee_share}}</td>
+									<td>{{$pay_reg->hdmf_employer_share}}</td>
+									<td>{{$pay_reg->phic_employee_share}}</td>
+									<td>{{$pay_reg->phic_employer_share}}</td>
+									<td>{{$pay_reg->mpf_employee_share}}</td>
+									<td>{{$pay_reg->mpf_employer_share}}</td>
+									<td>{{$pay_reg->statutory_total}}</td>
+									<td>{{$pay_reg->taxable_deductible_total}}</td>
+									<td>{{$pay_reg->net_taxable_income}}</td>
+									<td>{{$pay_reg->withholding_tax}}</td>
+									<td>{{$pay_reg->deminimis}}</td>
+									<td>{{$pay_reg->other_allowances_basic_pay}}</td>
+									<td>{{$pay_reg->subliq}}</td>
+									{{-- @php
+										$ids = [];
+									@endphp --}}
+									@foreach($allowances as $as => $total_allow)
+										@php
+											$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
+											$allllowance = 0;
+											foreach($ids as $idffff)
+											{
+												$alll = ($idffff->pay_allowances)->where('allowance_id',$total_allow->allowance_id)->sum('amount');
+												$allllowance = $allllowance + $alll;
+											}
+											
+										@endphp
+									<td>{{number_format($allllowance,2)}}</td>
+									@endforeach
+									<td>{{$pay_reg->nontaxable_benefits_total}}</td>
+									@foreach($instructions as $instruction)
+										@php
+											$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
+											$instruction_total = 0;
+											foreach($ids as $idffff)
+											{
+												$instruction_tot = ($idffff->pay_instructions)->where('instruction_name',$instruction->instruction_name)->sum('amount');
+												$instruction_total = $instruction_total + $instruction_tot;
+											}
+										@endphp
+									<td>{{number_format($instruction_total,2)}}</td>
+									@endforeach
+									@foreach($loans as $loans_al)
+										@php
+											$total_loans = 0;
+											$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
+											foreach($ids as $idffff)
+											{
+												$toloans = ($idffff->pay_loan)->where('loan_type_id',$loans_al->loan_type_id)->sum('amount');
+												$total_loans = $total_loans + $toloans;
+											}
+										@endphp
+									<td>{{number_format($total_loans,2)}}</td>
+									@endforeach
+									<td>{{$pay_reg->nontaxable_deductible_benefits_total}}</td>
+									<td>{{$pay_reg->gross_pay}}</td>
+									<td>{{$pay_reg->deductions_total}}</td>
+									<td>{{$pay_reg->netpay}}</td>
+								</tr>
+								@endforeach
+							</tbody>
+							</table>
+						</div>
 					</div>
+					
+					
 				</div>
 			</div>
-			<div class="table-responsive">
-				<table class="table table-db table-hover table-bordered">
-				  <thead>
-					  <tr>
-						  <th>Employee No</th>
-						  <th>Last Name</th>
-						  <th>First Name</th>
-						  <th>Middle Name</th>
-						  <th>Cost Center</th>
-						  <th>Department</th>
-						  <th>Account No</th>
-						  <th>Tax Status</th>
-						  <th>DAYS RENDERED</th>
-						  <th>BASIC PAY</th>
-						  <th>LH ND</th>
-						  <th>LH ND AMOUNT</th>
-						  <th>LH ND GE</th>
-						  <th>LH ND GE AMOUNT</th>
-						  <th>LH OT</th>
-						  <th>LH OT Amount</th>
-						  <th>LH OT OVER 8</th>
-						  <th>LH OT OVER 8 AMOUNT</th>
-						  <th>REG ND</th>
-						  <th>REG ND AMOUNT</th>
-						  <th>REG OT</th>
-						  <th>REG OT AMOUNT</th>
-						  <th>REG OT ND</th>
-						  <th>REG OT ND AMOUNT</th>
-						  <th>RST ND</th>
-						  <th>RST ND AMOUNT</th>
-						  <th>RST ND GE</th>
-						  <th>RST ND GE AMOUNT</th>
-						  <th>RST OT</th>
-						  <th>RST OT AMOUNT</th>
-						  <th>RST OT OVER 8</th>
-						  <th>RST OT OVER 8 AMOUNT</th>
-						  <th>OVERTIME TOTAL</th>
-						  {{-- <th>PL</th>
-						  <th>PL AMOUNT</th>
-						  <th>SL</th>
-						  <th>SL AMOUNT</th>
-						  <th>VL</th>
-						  <th>VL AMOUNT</th>
-						  <th>LEAVE AMOUNT TOTAL</th> --}}
-						  @foreach($salary_adjustments as $salary_adjustment)
-						  <th>{{$salary_adjustment->name}}</th>
-						  @endforeach
-						  <th>Total SALARY ADJUSTMENT</th>
-						  <th>TAXABLE BENEFITS TOTAL</th>
-						  <th>GROSS TAXABLE INCOME</th>
-						  <th>DAYS ABSENT</th>
-						  <th>ABSENT AMOUNT</th>
-						  <th>TARDINESS TOTAL</th>
-						  <th>TARDINESS AMOUNT</th>
-						  <th>UNDERTIME TOTAL</th>
-						  <th>UNDERTIME AMOUNT</th>
-						  <th>SSS EC</th>
-						  <th>SSS EMPLOYEE SHARE</th>
-						  <th>SSS EMPLOYER SHARE</th>
-						  <th>HDMF EMPLOYEE SHARE</th>
-						  <th>HDMF EMPLOYER SHARE</th>
-						  <th>PHIC EMPLOYEE SHARE</th>
-						  <th>PHIC EMPLOYER SHARE</th>
-						  <th>MPF EMPLOYEE SHARE</th>
-						  <th>MPF EMPLOYER SHARE</th>
-						  <th>STATUTORY TOTAL</th>
-						  <th>TAXABLE DEDUCTIBLE TOTAL</th>
-						  <th>NET TAXABLE INCOME</th>
-						  <th>WITHHOLDING TAX</th>
-						  <th>DEMINIMIS</th>
-						  <th>Other Allowances</th>
-						  <th>SUBLIQ</th>
-						  {{-- <th>DEMINIMIS ADJUSTMENT</th>
-						  <th>LOAD ALLOWANCE</th>
-						  <th>OTHER ALLOWANCES</th>
-						  <th>OTHER NTA</th>
-						  <th>SSS LOAN REFUND</th>
-						  <th>SUBLIQ</th> --}}
-						  @foreach($allowances as $total_allow)
-						  <th>{{$total_allow->allowance_type->name}}</th>
-						  @endforeach
-						  {{-- <th>OTHER ALLOWANCES</th> --}}
-						  <th>NONTAXABLE BENEFITS TOTAL</th>
-						  
-						  {{-- <th>CANTEEN</th>
-						  <th>EMERGENCY</th>
-						  <th>HDMF CALAMITY LOAN</th>
-						  <th>HDMF CONTRIBUTION UPGRADE</th>
-						  <th>HDMF LOAN</th>
-						  <th>MOTORCYCLE LOAN</th>
-						  <th>RICE LOAN</th>
-						  <th>SSS CALAMITY LOAN</th>
-						  <th>SSS LOAN</th>
-						  <th>STAFF HOUSE</th>
-						  <th>WESLA LOAN</th> --}}
-						  @foreach($instructions as $ins)
-						  <th>{{$ins->instruction_name}}</th>
-						  @endforeach
-						  {{-- <th>Others</th> --}}
-						  @foreach($loans as $loans_al)
-						  <th>{{$loans_al->loan_type->loan_name}}</th>
-						  @endforeach
-						  <th>NONTAXABLE DEDUCTIBLE BENEFITS TOTAL</th>
-						  <th>GROSS PAY</th>
-						  <th>DEDUCTIONS TOTAL</th>
-						  <th>NETPAY</th>
-					  </tr>
-				  </thead>
-				  <tbody>
-					@foreach($pay_registers as $key => $pay_reg)
-					<tr>
-						<td>{{$pay_reg->employee_no}}</td>
-						<td>{{$pay_reg->employee->last_name}}</td>
-						<td>{{$pay_reg->employee->first_name}}</td>
-						<td>{{$pay_reg->employee->middle_name}}</td>
-						<td>{{$pay_reg->cost_center}}</td>
-						<td>{{$pay_reg->employee->department->name}}</td>
-						<td>{{$pay_reg->employee->account_number}}</td>
-						<td>{{$pay_reg->tax_status}}</td>
-						<td>{{$pay_reg->days_rendered}}</td>
-						<td>{{$pay_reg->basic_pay}}</td>
-						<td>{{$pay_reg->lh_nd}}</td>
-						<td>{{$pay_reg->lh_nd_amount}}</td>
-						<td>{{$pay_reg->lh_nd_ge}}</td>
-						<td>{{$pay_reg->lh_nd_ge_amount}}</td>
-						<td>{{$pay_reg->lh_ot}}</td>
-						<td>{{$pay_reg->lh_ot_amount}}</td>
-						<td>{{$pay_reg->lh_ot_ge}}</td>
-						<td>{{$pay_reg->lh_ot_ge_amount}}</td>
-						<td>{{$pay_reg->reg_nd}}</td>
-						<td>{{$pay_reg->reg_nd_amount}}</td>
-						<td>{{$pay_reg->reg_ot}}</td>
-						<td>{{$pay_reg->reg_ot_amount}}</td>
-						<td>{{$pay_reg->reg_ot_nd}}</td>
-						<td>{{$pay_reg->reg_ot_nd_amount}}</td>
-						<td>{{$pay_reg->rst_nd}}</td>
-						<td>{{$pay_reg->rst_nd_amount}}</td>
-						<td>{{$pay_reg->rst_nd_ge}}</td>
-						<td>{{$pay_reg->rst_nd_ge_amount}}</td>
-						<td>{{$pay_reg->rst_ot}}</td>
-						<td>{{$pay_reg->rst_ot_amount}}</td>
-						<td>{{$pay_reg->rst_ot_ge}}</td>
-						<td>{{$pay_reg->rst_ot_ge_amount}}</td>
-						<td>{{$pay_reg->ot_total}}</td>
-						@foreach($salary_adjustments as $as => $salary_adjustment)
-							@php
-								$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
-								$salaryaddjustment_all = 0;
-								foreach($ids as $idffff)
-								{
-									$alll = ($idffff->salary_adjustments_data)->where('name',$salary_adjustment->name)->sum('amount');
-									$salaryaddjustment_all = $salaryaddjustment_all + $alll;
-								}
-								
-							@endphp
-						<td>{{number_format($salaryaddjustment_all,2)}}</td>
-						@endforeach
-						<td>{{$pay_reg->salary_adjustment}}</td>
-						<td>{{$pay_reg->taxable_benefits_total}}</td>
-						<td>{{$pay_reg->gross_taxable_income}}</td>
-						<td>{{$pay_reg->days_absent}}</td>
-						<td>{{$pay_reg->absent_amount}}</td>
-						<td>{{$pay_reg->tardiness_total}}</td>
-						<td>{{$pay_reg->tardiness_amount}}</td>
-						<td>{{$pay_reg->undertime_total}}</td>
-						<td>{{$pay_reg->undertime_amount}}</td>
-						<td>{{$pay_reg->sss_ec}}</td>
-						<td>{{$pay_reg->sss_employee_share}}</td>
-						<td>{{$pay_reg->sss_employer_share}}</td>
-						<td>{{$pay_reg->hdmf_employee_share}}</td>
-						<td>{{$pay_reg->hdmf_employer_share}}</td>
-						<td>{{$pay_reg->phic_employee_share}}</td>
-						<td>{{$pay_reg->phic_employer_share}}</td>
-						<td>{{$pay_reg->mpf_employee_share}}</td>
-						<td>{{$pay_reg->mpf_employer_share}}</td>
-						<td>{{$pay_reg->statutory_total}}</td>
-						<td>{{$pay_reg->taxable_deductible_total}}</td>
-						<td>{{$pay_reg->net_taxable_income}}</td>
-						<td>{{$pay_reg->withholding_tax}}</td>
-						<td>{{$pay_reg->deminimis}}</td>
-						<td>{{$pay_reg->other_allowances_basic_pay}}</td>
-						<td>{{$pay_reg->subliq}}</td>
-						{{-- @php
-							$ids = [];
-						@endphp --}}
-						@foreach($allowances as $as => $total_allow)
-							@php
-								$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
-								$allllowance = 0;
-								foreach($ids as $idffff)
-								{
-									$alll = ($idffff->pay_allowances)->where('allowance_id',$total_allow->allowance_id)->sum('amount');
-									$allllowance = $allllowance + $alll;
-								}
-								
-							@endphp
-						<td>{{number_format($allllowance,2)}}</td>
-						@endforeach
-						<td>{{$pay_reg->nontaxable_benefits_total}}</td>
-						@foreach($instructions as $instruction)
-							@php
-								$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
-								$instruction_total = 0;
-								foreach($ids as $idffff)
-								{
-									$instruction_tot = ($idffff->pay_instructions)->where('instruction_name',$instruction->instruction_name)->sum('amount');
-									$instruction_total = $instruction_total + $instruction_tot;
-								}
-							@endphp
-						<td>{{number_format($instruction_total,2)}}</td>
-						  @endforeach
-						@foreach($loans as $loans_al)
-							@php
-								$total_loans = 0;
-								$ids = $pay_register_ids_data->where('employee_no',$pay_reg->employee_no);
-								foreach($ids as $idffff)
-								{
-									$toloans = ($idffff->pay_loan)->where('loan_type_id',$loans_al->loan_type_id)->sum('amount');
-									$total_loans = $total_loans + $toloans;
-								}
-							@endphp
-						<td>{{number_format($total_loans,2)}}</td>
-						  @endforeach
-						<td>{{$pay_reg->nontaxable_deductible_benefits_total}}</td>
-						<td>{{$pay_reg->gross_pay}}</td>
-						<td>{{$pay_reg->deductions_total}}</td>
-						<td>{{$pay_reg->netpay}}</td>
-					</tr>
-					@endforeach
-				  </tbody>
-				</table>
-			  </div>
 		</div>
 	</div>
 
