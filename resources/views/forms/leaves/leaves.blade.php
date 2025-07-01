39a975a42095f53fecca1a625a0f67ddd276ab91
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-07-01 16:44:46 +0800

    done

--- resources/views/forms/leaves/leaves.blade.php
+++ resources/views/forms/leaves/leaves.blade.php
@@ -1171,7 +1171,7 @@
                       </td>
                       <td>
                         @if($employee_leave->leave_file)
-                          <a href="storage/{{ $employee_leave->leave_file }}" target="_blank">{{ $employee_leave->leave_file }}</a>
+                          <a href="storage/{{ urlencode($employee_leave->leave_file) }}" target="_blank">{{ $employee_leave->leave_file }}</a>
                         @elseif($employee_leave->attachment)
                         <a href="{{ url($employee_leave->attachment) }}" target="_blank">{{ $employee_leave->attachment }}</a>
                         @else
