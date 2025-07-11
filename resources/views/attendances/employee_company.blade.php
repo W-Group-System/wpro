380b9ff97619aab16f6fc154c426f22951e7782b
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-07-11 15:14:18 +0800

    Done

--- resources/views/attendances/employee_company.blade.php
+++ resources/views/attendances/employee_company.blade.php
@@ -747,7 +747,16 @@
                                                                 $late = ($schedule_hours/2)-$work;
                                                                 if($work < $schedule_hours/2)
                                                                 {
-                                                                    $late = (double) number_format(($schedule_hours/2 - $work),2) *60;
+                                                                    $default_halfday_hrs = 4;
+                                                                    if ($schedule_hours <= $default_halfday_hrs)
+                                                                    {
+                                                                        $late = 0;
+                                                                    }
+                                                                    else
+                                                                    {
+                                                                        $late = (double) number_format(($schedule_hours/2 - $work),2) *60;
+                                                                    }
+                                                                    
                                                                     $undertime_hrs = 0;
                                                                 } 
                                                             }
