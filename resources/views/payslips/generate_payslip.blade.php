7dda9fbc0accba3ac421ba092e9288a2ccfa12ac
Mark Ian <bautistamarkian083@gmail.com>
2025-08-16 14:19:43 +0800

    add sh total and amount in payslip

--- resources/views/payslips/generate_payslip.blade.php
+++ resources/views/payslips/generate_payslip.blade.php
@@ -255,6 +255,41 @@
                         <td style="text-align: right;">{{number_format($payroll->rst_ot_ge_amount ,2)}}</td>
                     </tr>
                     @endif
+                    @if($payroll->sh_amount > 0)
+                    <tr>
+                        <td style=""> - SH ND</td>
+                        <td style="">{{number_format($payroll->sh_nd,2)}}</td>
+                        <td style="text-align: right;">{{number_format($payroll->sh_amount ,2)}}</td>
+                    </tr>
+                    @endif
+                    @if($payroll->sh_ge_amount > 0)
+                    <tr>
+                        <td style=""> - SH GE</td>
+                        <td style="">{{number_format($payroll->sh_ge,2)}}</td>
+                        <td style="text-align: right;">{{number_format($payroll->sh_ge_amount ,2)}}</td>
+                    </tr>
+                    @endif
+                    @if($payroll->sh_nd_ge_amount > 0)
+                    <tr>
+                        <td style=""> - SH GE</td>
+                        <td style="">{{number_format($payroll->sh_nd_ge,2)}}</td>
+                        <td style="text-align: right;">{{number_format($payroll->sh_nd_ge_amount ,2)}}</td>
+                    </tr>
+                    @endif
+                    @if($payroll->sh_ot_amount > 0)
+                    <tr>
+                        <td style=""> - SH OT</td>
+                        <td style="">{{number_format($payroll->sh_ot,2)}}</td>
+                        <td style="text-align: right;">{{number_format($payroll->sh_ot_amount ,2)}}</td>
+                    </tr>
+                    @endif
+                    @if($payroll->sh_ot_ge_amount > 0)
+                    <tr>
+                        <td style=""> - SH OT GE</td>
+                        <td style="">{{number_format($payroll->sh_ot_ge,2)}}</td>
+                        <td style="text-align: right;">{{number_format($payroll->sh_ot_ge_amount ,2)}}</td>
+                    </tr>
+                    @endif
                     @foreach($payroll->salary_adjustments_data as $adjustments)
                     <tr>
                         <td style=""  colspan='2'>{{$adjustments->name}}</td>
