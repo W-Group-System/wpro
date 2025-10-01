4e3f18ea685a3463d13a21e1e00d6beac55dfe72
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-10-01 11:28:06 +0800

    add softdelete

--- app/ExitResign.php
+++ app/ExitResign.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 use OwenIt\Auditing\Contracts\Auditable; 
 
 class ExitResign extends Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
+    use SoftDeletes;
 
     public function Employee()
     {
