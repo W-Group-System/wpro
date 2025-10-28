ad9d8def681df1200241cdf545b1e7583fc355fd
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-10-28 10:31:46 +0800

    done

--- app/Http/Controllers/TimekeepingDashboardController.php
+++ app/Http/Controllers/TimekeepingDashboardController.php
@@ -530,7 +530,7 @@ class TimekeepingDashboardController extends Controller
                 $q->where('department_id', $department_data);
             })
             ->where('status','Active')
-            // ->where('employee_code','A3179024')
+            // ->where('employee_code','A3188125')
             // ->where('employee_code','A3189225')
             // ->where('employee_code','A2110025')
             // ->where('employee_code','A192524')
