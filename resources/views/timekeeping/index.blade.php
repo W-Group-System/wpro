801b82246145dc7f76d47bc15307c339696ef9ec
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-11-21 09:44:52 +0800

    done

--- resources/views/timekeeping/index.blade.php
+++ resources/views/timekeeping/index.blade.php
@@ -622,7 +622,10 @@
                                                                     {
                                                                         $work_ot = round(((strtotime($final_time_out) - strtotime($final_time_in)) / 3600), 2);
                                                                         $break_hrs = ($employee->approved_ots)->first();
-                                                                        $work_ot = $work_ot-$break_hrs->break_hrs;
+                                                                        if ($break_hrs)
+                                                                        {
+                                                                            $work_ot = $work_ot-$break_hrs->break_hrs;
+                                                                        }
                                                                         if ($work_ot >= 2)
                                                                         {
                                                                             $restday_ot = 8;
@@ -1895,7 +1898,10 @@
                                                                         {
                                                                             $work_ot = round(((strtotime($final_time_out) - strtotime($final_time_in)) / 3600), 2);
                                                                             $break_hrs = ($employee->approved_ots)->first();
-                                                                            $work_ot = $work_ot-$break_hrs->break_hrs;
+                                                                            if ($break_hrs)
+                                                                            {
+                                                                                $work_ot = $work_ot-$break_hrs->break_hrs;
+                                                                            }
                                                                             if ($work_ot >= 2)
                                                                             {
                                                                                 $restday_ot = 8;
