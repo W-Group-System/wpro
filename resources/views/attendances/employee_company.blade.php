c24add26f6610a4782bee8fdc40dc7c4ea412416
Schultz Henry Boado <schultzxhenry@gmail.com>
2025-12-09 17:06:13 +0800

    done adding rule for daily rate employee in legal holiday

--- resources/views/attendances/employee_company.blade.php
+++ resources/views/attendances/employee_company.blade.php
@@ -1271,6 +1271,25 @@
                                                     }
                                                   
                                                 }
+                                                // Paid Legal Holiday for Daily Rate Employee
+                                                if($emp->work_description == "Non-Monthly")
+                                                {
+                                                    if($check_if_holiday == "Special Holiday")
+                                                    {
+                                                        $abs = 1;
+                                                    }
+                                                    else 
+                                                    {
+                                                        $abs = 0;
+
+                                                        if($employee_schedule_before)
+                                                        {
+                                                            $work = (strtotime($date_r." ".$employee_schedule_before->time_out_to)-strtotime($date_r." ".$employee_schedule_before->time_in_to))/3600;
+                                                            $work = $work-1;
+                                                        }
+                                                    }
+                                                }
+
                                                 if($abs == 1)
                                                 {
                                                     $work = 0;
