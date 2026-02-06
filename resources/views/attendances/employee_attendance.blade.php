50353dfb5abb83b2ece38661957169cc910c0370
JunJihad <junjihadbarroga@gmail.com>
2026-02-06 11:34:23 +0800

    done

--- resources/views/attendances/employee_attendance.blade.php
+++ resources/views/attendances/employee_attendance.blade.php
@@ -743,6 +743,8 @@
                                     $sh_ot_ge=0;
                                     $lh_ot=0;
                                     $lh_ot_ge=0;
+                                    $lh_ot_nd=0;
+                                    $lh_ot_nd_ge=0;
                                     if($check_if_holiday)
                                     {
                                         $late = 0;
@@ -777,6 +779,66 @@
                                                 $lh_ot_ge = $approved_overtime_hrs-8;
                                             }
                                         }
+
+                                        if($employee_schedule)
+                                                            {
+                                                                $time_start_string = strtotime($time_start);
+                                                                $time_end_string = strtotime($time_end);
+                                                                $schedule_out = strtotime($date_r." ".$employee_schedule->time_out_to);
+                                                                $schedule_in = strtotime($date_r." ".$employee_schedule->time_in_to);
+                                                                
+                                                                if(($schedule_out) < ($schedule_in))
+                                                                {
+                                                                    
+                                                                    $schedule_out = strtotime($date_r." ".$employee_schedule->time_out_to)+86400;
+                                                                }
+                                                                if($time_end_string>$schedule_out)
+                                                                {
+                                                                    $lh_ot_nd =  night_difference_per_company(date('Y-m-d H:i',$schedule_in),date('Y-m-d H:i',$schedule_out));
+                                                                    $lh_ot_use = $lh_ot_nd;
+                                                                    if($lh_ot_nd >=4.5 )
+                                                                    {   
+                                                                        $schedule_hours = ((($schedule_out)-($schedule_in))/3600);
+                                                                        if($schedule_hours > 8)
+                                                                        {
+                                                                        $lh_ot_nd = $lh_ot_nd-1;
+                                                                        }
+                                                                    }
+                                                                    
+                                                                    if ($rest == "RESTDAY")
+                                                                    {
+                                                                        $lh_ot_nd = 0;
+                                                                        $rst_lh_nd_ge = night_difference_per_company(date('Y-m-d H:i',$schedule_in),$time_end)-$lh_ot_use;
+                                                                        if($rst_lh_nd_ge <0)
+                                                                        {
+                                                                            $rst_lh_nd_ge=0;
+                                                                        }
+                                                                    }
+                                                                    else 
+                                                                    {
+                                                                        $rst_lh_nd_ge = 0;
+                                                                        $lh_ot_nd_ge =night_difference_per_company(date('Y-m-d H:i',$schedule_in),$time_end)-$lh_ot_use;
+                                                                        if($lh_ot_nd_ge <0)
+                                                                        {
+                                                                            $lh_ot_nd_ge=0;
+                                                                        }
+                                                                    }
+                                                                }
+                                                                else {
+                                                                    $lh_ot_nd =  night_difference_per_company(date('Y-m-d H:i',$schedule_in),$time_end);
+                                                                    if($lh_ot_nd >=4.5 )
+                                                                    {   
+                                                                        $schedule_hours = ((($schedule_out)-($schedule_in))/3600);
+                                                                        if($schedule_hours > 8)
+                                                                        {
+                                                                        $lh_ot_nd = $lh_ot_nd-1;
+                                                                        }
+                                                                    }
+                                                                }
+                                                             
+                                                            }
+                                        
+
                                     }
                                     $subtotal_abs += $abs;
                                     $subtotal_leave_w_pay += $leave_count;
@@ -805,8 +867,8 @@
                                     <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][rst_nd_over_eight]" value="0.00">0.00</td> {{-- RST ND > 8 --}}
                                     <td @if($lh_ot>0) class='bg-warning'@endif><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_ot]" value="0.00">{{ number_format($lh_ot,2) }}</td> {{-- LH OT --}}
                                     <td @if($lh_ot_ge>0) class='bg-warning'@endif><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_ot_over_eight]" value="0.00">{{ number_format($lh_ot_ge,2) }}</td> {{-- LH OT > 8 --}}
-                                    <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_nd]" value="0.00">0.00</td> {{-- LH ND --}}
-                                    <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_nd_over_eight]" value="0.00">0.00</td> {{-- LH ND > 8 --}}
+                                    <td @if($lh_ot_nd>0) class='bg-warning'@endif><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_nd]" value="{{ $lh_ot_nd }}">{{ number_format($lh_ot_nd,2) }}</td> {{-- LH ND --}}
+                                    <td @if($lh_ot_nd_ge>0) class='bg-warning'@endif><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][lh_nd_over_eight]" value="{{ $lh_ot_nd_ge }}">{{ number_format($lh_ot_nd_ge,2) }}</td> {{-- LH ND > 8 --}}
                                     <td @if($sh_ot>0) class='bg-warning'@endif><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][sh_ot]" value="0.00">{{ number_format($sh_ot,2) }}</td> {{-- SH OT --}}
                                     <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][sh_ot_over_eight]" value="0.00">0.00</td> {{-- SH OT > 8 --}}
                                     <td><input type="hidden" name="employees[{{ $emp->employee_code }}][{{$date_r}}][sh_nd]" value="0.00">0.00</td> {{-- SH ND --}}
